a5177af307a412c39700bae207a1b355ad1a9ff6
Sasha <q.dev.null@gmail.com>
2021-04-20 02:59:07 +0200

    Simplify the default logic

--- elan/words.py
+++ elan/words.py
@@ -1,114 +1,108 @@
-#!/usr/bin/python
-# coding=utf-8
-
-from random import choice
 import json
-
-# TODO: check version?
-import string 
 
 
 class WordsChecker(object):
-	def __init__(self, filename, chapter, option, strict = False):
-		super(WordsChecker, self).__init__()
-		self.option = option
-		self.vocabulary = []
-		self.vtype = 'words' if 'word' in self.option else 'verbs'
-		self.read_file(filename, chapter)
-		# For interactive mode
-		# TODO: try to avoid this
-		self.stack = self.vocabulary.keys()
-		# Get Default Method
-		self.comp = self.compare_strict if strict else self.compare_normal
+    def __init__(self, filename, chapter, option, strict=False):
+        super(WordsChecker, self).__init__()
+        self.option = option
+        self.vocabulary = []
+        self.vtype = 'words' if 'word' in self.option else 'verbs'
+        self.read_file(filename, chapter)
+        # For interactive mode
+        # TODO: try to avoid this
+        self.stack = self.vocabulary.keys()
+        # Get Default Method
+        self.comp = self.compare_strict if strict else self.compare_normal
 
-		self.symbols_table = dict(zip(u"éàèùâêîôûç", "eaeuaeiouc"))
-		# self.symbols_table = dict(zip(map(ord, u"éàèùâêîôûç"), "eaeuaeiouc"))
-		print self.symbols_table
+        self.symbols_table = dict(zip(u"éàèùâêîôûç", "eaeuaeiouc"))
+        # self.symbols_table = dict(zip(map(ord, u"éàèùâêîôûç"), "eaeuaeiouc"))
+        print(self.symbols_table)
 
+    def convert_for_verbs(self):
+        if 'word' in self.option:
+            return
 
+        # Fix the issue with muptiple keys
+        res = {
+            self.vocabulary[k][w]: k + '.\n' + w + ' ... '
+            for k in self.vocabulary for w in self.vocabulary[k]
+        }
 
-	def convert_for_verbs(self):
-		if 'word' in self.option:
-			return
+        # This is a version to debug
+        # res = {}
+        # for k in self.vocabulary:
+        #   print k
+        #   for w in self.vocabulary[k]:
+        #       print self.vocabulary[k][w]
+        #       res.update({self.vocabulary[k][w]: k  + '.\n' + w + ' ... ' })
 
-		# Fix the issue with muptiple keys
-		res = {self.vocabulary[k][w] : k  + '.\n' + w + ' ... ' for k in self.vocabulary for w in self.vocabulary[k]}
+        self.vocabulary = res
 
-		# This is a version to debug
-		# res = {}
-		# for k in self.vocabulary:
-		# 	print k
-		# 	for w in self.vocabulary[k]:
-		# 		print self.vocabulary[k][w]
-		# 		res.update({self.vocabulary[k][w]: k  + '.\n' + w + ' ... ' })
+    def read_file(self, filename, chapter):
+        with open(filename) as f:
+            data = json.load(f)
 
-		self.vocabulary = res
+        self.vocabulary = {}
 
-	def read_file(self, filename, chapter):
-		with open(filename) as f:
-			data = json.load(f)
+        for c in data:
+            if chapter in c:
+                self.vocabulary.update(data[c][self.vtype])
 
-		self.vocabulary = {}
+        self.convert_for_verbs()
 
-		for c in data:
-			if chapter in c:
-				self.vocabulary.update(data[c][self.vtype])
+    def test_dictionary(self, wdict):
+        weak_words = {}
+        for foreign, native in wdict.iteritems():
+            suggestion = input(f'Type in translation for:\n{native}')
+            if suggestion != foreign:
+                weak_words[foreign] = native
+        return weak_words
 
-		self.convert_for_verbs()
+    def test_words(self):
+        test_words = self.vocabulary
+        while test_words:
+            test_words = self.test_dictionary(test_words)
 
-	def test_dictionary(self, wdict):
-		weak_words = {}
-		for foreign, native in wdict.iteritems():
-			suggestion = raw_input('Type in translation for ' + native + ' : ')
-			if suggestion != foreign: weak_words[foreign] = native
-		return weak_words
+        # if weak_words: print 'List ouf your weak words:'
+        # for k in weak_words: print k
+        print('Congrats! Your training is over')
 
-	def test_words(self):
-		test_words = self.vocabulary
-		while test_words:
-			test_words = self.test_dictionary(test_words)
+    def compare_normal(self, foreign, translation):
+        # zip(u"éàèùâêîôûç", "eaeuaeiouc")
 
-		# if weak_words: print 'List ouf your weak words:'
-		# for k in weak_words: print k
-		print 'Congrats! Your training is over'
+        fr = (u"".join(foreign)).translate(self.symbols_table)
+        tr = (u"".join(translation)).translate(self.symbols_table)
 
-	def compare_normal(self, foreign, translation):
-		# zip(u"éàèùâêîôûç", "eaeuaeiouc")
+        print(tr, fr)
+        return fr.lower() == tr.lower()
 
-		fr = (u"".join(foreign)).translate(self.symbols_table)
-		tr = (u"".join(translation)).translate(self.symbols_table)
+    def compare_strict(self, foreign, translation):
+        return foreign == translation
 
-		print tr, fr
-		return fr.lower() == tr.lower()
+    def interactive(self, q, a, c, s):
+        if not self.vocabulary:
+            return
 
+        foreign, native = self.stack[0], self.vocabulary[self.stack[0]]
 
-	def compare_strict(self, foreign, translation):
-		return foreign == translation
+        suggestion = a.get()
+        if not self.comp(foreign, suggestion):
+            c.set(foreign)
+            s.set(suggestion)
+            self.stack.append(foreign)
+        else:
+            self.stack.pop(0)
+            c.set('')
+            s.set('')
 
+        if not self.stack:
+            q.set("That's all")
+            a.set("Congrats, you've finished it!")
+            c.set("Bye!!")
+            return
 
-	def interactive(self, q, a, c, s):
-		if not self.vocabulary: return
+        # print self.stack
 
-		foreign, native = self.stack[0], self.vocabulary[self.stack[0]]
-
-		suggestion = a.get()
-		if not self.comp(foreign, suggestion):
-			c.set(foreign)
-			s.set(suggestion)
-			self.stack.append(foreign)
-		else:
-			self.stack.pop(0)
-			c.set('')
-			s.set('')
-
-		if not self.stack:
-			q.set("That's all")
-			a.set("Congrats, you've finished it!")
-			c.set("Bye!!")
-			return
-
-		# print self.stack
-
-		foreign, native = self.stack[0], self.vocabulary[self.stack[0]]
-		q.set(native)
-		a.set('')
+        foreign, native = self.stack[0], self.vocabulary[self.stack[0]]
+        q.set(native)
+        a.set('')
