86b551ccc17416a1fdcafb6b433bba0edab48e19
Sasha <q.dev.null@gmail.com>
2021-04-20 02:51:09 +0200

    Update the wordchecker

--- tests/test_wordchecker.py
+++ tests/test_wordchecker.py
@@ -4,31 +4,25 @@ import unittest
 from elan.words import WordsChecker
 
 
-
 class TestWordsChecker(unittest.TestCase):
 
+    def setUp(self):
+        self.file = 'testfile.json'
+        data = {
+            "Chapter1": {
+                "words": {
+                    "aaa": "xxx",
+                    "bbb": "yyy"
+                }
+            }
+        }
 
-	def setUp(self):
-		self.file = 'testfile.json'
-		data = {
-			"Chapter1": {
-				"words":{
-					"aaa": "xxx",
-					"bbb": "yyy"
-				}
-			}
-		}
+        with open(self.file, 'w') as outfile:
+            json.dump(data, outfile)
 
-		with open(self.file, 'w') as outfile:
-			json.dump(data, outfile)
+    def test_checks_words(self):
+        checker = WordsChecker(self.file, "Chapter1", {"word": ""})
+        checker.test_words()
 
-
-	def test_checks_words(self):
-		checker = WordsChecker(self.file, "Chapter1", {"word": ""})
-		checker.test_words()
-
-
-	def tearDown(self):
-		os.remove(self.file)
-
-
+    def tearDown(self):
+        os.remove(self.file)
