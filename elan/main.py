df776c21cdc8519d510760325a407a7252762738
kqf <q.dev.null@gmail.com>
2021-04-23 18:01:07 +0200

    Clean the entrypoint (#6)
    * Rename the main entrypoint
    
    * Remove the unused functions

--- elan/main.py
+++ elan/main.py
@@ -3,6 +3,25 @@ import click
 from ui import gui, cmline
 
 
+def needs_file(argv):
+    for f in argv:
+        if '.json' in f:
+            return f
+
+
+"""
+Usage:
+
+words:
+    elan/./elan.py "Lesson 1" --words --gui examples/vocabulary.json
+
+verbs:
+    elan/./elan.py "Lesson 1" --verbs --gui examples/vocabulary.json
+
+.PHONY: words verbs
+"""
+
+
 @click.command()
 @click.option("--infile", type=click.Path(exists=True))
 @click.option("--mode", type=click.Choice(["gui", "cli"]), default="cli")
