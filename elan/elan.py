030bbc47625f625ab1d491db4b67815ad7e3ff50
kqf <q.dev.null@gmail.com>
2021-04-23 17:57:59 +0200

    Fix the cli script (#5)
    * Update the requirements list
    
    * Fix the cli arguments for the script

--- elan/elan.py
+++ elan/elan.py
@@ -1,4 +1,4 @@
-import sys
+import click
 
 from ui import gui, cmline
 
@@ -22,19 +22,17 @@ verbs:
 """
 
 
-def main():
-    infile = needs_file(sys.argv)
-    print(infile)
-    msg = "You should specify the path to a .json file with your vocabulary"
-
-    if not infile:
-        raise IOError(msg)
-
-    if 'gui' not in ''.join(sys.argv):
-        cmline(infile, sys.argv[1], sys.argv[2])
+@click.command()
+@click.option("--infile", type=click.Path(exists=True))
+@click.option("--mode", type=click.Choice(["gui", "cli"]), default="cli")
+@click.option("--lesson", type=str)
+@click.option("--objective", type=str)
+def main(infile, mode, lesson, objective):
+    if mode == "cli":
+        cmline(infile, lesson, objective)
         return
 
-    gui(infile, sys.argv[1], sys.argv[2])
+    gui(infile, lesson, objective)
 
 
 if __name__ == '__main__':
