4502ca47d764474fbf9bdd6db8e03b53d5b504ab
Sasha <q.dev.null@gmail.com>
2021-04-26 05:27:56 +0200

    Fix the endpoints

--- elan/main.py
+++ elan/main.py
@@ -2,32 +2,19 @@ import click
 from elan.words import WordsChecker
 
 
-"""
-Usage:
-
-words:
-    elan/./elan.py "Lesson 1" --words --gui examples/vocabulary.json
-
-verbs:
-    elan/./elan.py "Lesson 1" --verbs --gui examples/vocabulary.json
-
-.PHONY: words verbs
-"""
-
-
 @click.command()
 @click.option("--infile", type=click.Path(exists=True))
 @click.option("--mode", type=click.Choice(["gui", "cli"]), default="cli")
 @click.option("--lesson", type=str)
 @click.option("--objective", type=str)
 def main(infile, mode, lesson, objective):
-    if mode == "cli":
-        w = WordsChecker(infile, lesson)
-        w.test_words()
+    if mode == "gui":
+        from ui import gui
+        gui(infile, lesson, objective)
         return
 
-    from ui import gui
-    gui(infile, lesson, objective)
+    w = WordsChecker(infile, lesson)
+    w.test_words()
 
 
 if __name__ == '__main__':
