3c6849048feeeab2cefe61f4d3d5a49f9ec9d9b4
Sasha <q.dev.null@gmail.com>
2021-04-27 17:44:18 +0200

    Remove the unittest module imports

--- tests/test_wordchecker.py
+++ tests/test_wordchecker.py
@@ -1,30 +1,27 @@
-import os
 import json
-import unittest
 import pytest
+import tempfile
+
 from elan.words import WordsChecker
 
 
-class TestWordsChecker(unittest.TestCase):
-
-    def setUp(self):
-        self.file = 'testfile.json'
-        data = {
-            "Chapter1": {
-                "words": {
-                    "aaa": "xxx",
-                    "bbb": "yyy"
-                }
+@pytest.fixture
+def infile():
+    data = {
+        "Chapter1": {
+            "words": {
+                "aaa": "xxx",
+                "bbb": "yyy"
             }
         }
-
-        with open(self.file, 'w') as outfile:
+    }
+    with tempfile.NamedTemporaryFile() as tmp:
+        with open(tmp, 'w') as outfile:
             json.dump(data, outfile)
+        yield tmp
 
-    @pytest.mark.skip("Fix the test logic")
-    def test_checks_words(self):
-        checker = WordsChecker(self.file, "Chapter1", {"word": ""})
-        checker.test_words()
 
-    def tearDown(self):
-        os.remove(self.file)
+@pytest.mark.skip("Fix the test logic")
+def test_checks_words(self):
+    checker = WordsChecker(self.file, "Chapter1", {"word": ""})
+    checker.test_words()
