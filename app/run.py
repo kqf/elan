515c6c716d78b847ba58e6f0738b1c67d9aad76a
Sasha <q.dev.null@gmail.com>
2021-05-05 21:51:57 +0200

    Add the basic webui

---
+++ app/run.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required, Length
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+class AnswerForm(Form):
+    translation = StringField('',
+                              validators=[Required(), Length(1, 16)])
+    submit = SubmitField('submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    translation = None
+    form = AnswerForm()
+    if form.validate_on_submit():
+        translation = form.translation.data
+        form.translation.data = ''
+    return render_template('index.html', form=form, translation=translation)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
