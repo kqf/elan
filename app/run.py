59ebef2409531f5d9d176d791bc45d2aa6d93ba0
Sasha <q.dev.null@gmail.com>
2021-06-24 09:16:15 +0200

    Fix the program logic

--- app/run.py
+++ app/run.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_bootstrap import Bootstrap
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_KEY'
+
 bootstrap = Bootstrap(app)
 
 
@@ -16,27 +18,45 @@ class AnswerForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    vocab = {
-        "this is a first message": "first",
-        "this is a second message": "second",
-        "this is a third message": "third",
-    }
+    if "tasks" not in session:
+        session["tasks"] = [
+            ("this is a first message", "first"),
+            ("this is a second message", "second"),
+            ("this is a third message", "third"),
+            ("this is a fourth message", "fourth"),
+        ]
 
     translation = None
     form = AnswerForm(form_type="inline")
-    for original, correct in vocab.items():
-        while translation == correct:
-            if form.validate_on_submit():
-                translation = form.translation.data
-                form.translation.data = ''
-                del vocab[original]
+    print(session["tasks"])
 
-            render_template(
-                'index.html',
-                form=form,
-                translation=translation,
-                original=original,
-            )
+    if len(session["tasks"]) == 0:
+        return render_template(
+            'index.html',
+            form=form,
+            translation=translation,
+            original="You are done",
+        )
+
+    original, correct = next(iter(session["tasks"]))
+
+    if form.translation.data is None:
+        return render_template(
+            'index.html',
+            form=form,
+            translation=translation,
+            original=original,
+        )
+
+    print(correct)
+
+    if form.validate_on_submit() and form.translation.data == correct:
+        translation = form.translation.data
+        form.translation.data = None
+        session["tasks"].pop(0)
+        original, correct = next(iter(session["tasks"]))
+        print(session["tasks"])
+        print("After pop", correct)
 
     return render_template(
         'index.html',
