a1c6a6089825105bc9b1f84dd8179a3531537279
kqf <q.dev.null@gmail.com>
2021-06-25 09:46:09 +0200

    Add the upload form (#22)
    * Ignore the .txt files
    
    * Add the upload form
    
    * Add the upload vocabulary

--- app/run.py
+++ app/run.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask, render_template, session
 from flask_bootstrap import Bootstrap
 from flask_session import Session
 
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
+
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
@@ -19,35 +22,46 @@ class AnswerForm(FlaskForm):
     submit = SubmitField('submit')
 
 
+class UploadForm(FlaskForm):
+    file = FileField('Consider uploading a new file')
+    upload = SubmitField('Upload')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if "tasks" not in session:
-        session["tasks"] = [
-            ("this is a first message", "first"),
-            ("this is a second message", "second"),
-            ("this is a third message", "third"),
-            ("this is a fourth message", "fourth"),
-        ]
+    upload = UploadForm()
 
-    form = None
+    if upload.file.data:
+        tasks = []
+        for line in upload.file.data.readlines():
+            print(type(line))
+            original, expected = line.decode("utf-8").split("|")
+            tasks.append((original.strip(), expected.strip()))
+            session["tasks"] = tasks
+
+    if "tasks" not in session:
+        session["tasks"] = []
+
+    prompt = None
     translation = None
     original = None
     correct = None
 
     if len(session["tasks"]) > 0:
-        form = AnswerForm(form_type="inline")
+        prompt = AnswerForm(form_type="inline")
         original, expected = next(iter(session["tasks"]))
-        correct = form.translation.data == expected
+        correct = prompt.translation.data == expected
 
     if correct:
         session["tasks"].pop(0)
 
     return render_template(
         'index.html',
-        form=form,
+        prompt=prompt,
         translation=translation,
         original=original,
         correct=correct,
+        upload=upload,
     )
 
 
