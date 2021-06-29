3ebaeb222eb9c4f86d23f0c697167e75a60a2d4e
kqf <q.dev.null@gmail.com>
2021-06-29 18:44:36 +0200

    Add the login form (#29)
    * Add the login form
    
    * Refactor the index.html
    
    * Add the login route
    
    * Fix the circular imports issue
    
    * Fix the indentation

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import Required
 
 
@@ -14,3 +14,10 @@ class AnswerForm(FlaskForm):
 class UploadForm(FlaskForm):
     file = FileField('Consider uploading a new file')
     upload = SubmitField('Upload')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[Required()])
+    password = PasswordField('Password', validators=[Required()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Submit')
