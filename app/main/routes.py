f8afeb8f44061c3807ae27fc71be02c5404e6aa3
kqf <q.dev.null@gmail.com>
2021-06-29 18:44:36 +0200

    Add the login form (#29)
    * Add the login form
    
    * Refactor the index.html
    
    * Add the login route
    
    * Fix the circular imports issue
    
    * Fix the indentation

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template, session
-from app.main.forms import UploadForm, AnswerForm
+from flask import render_template, session
+from flask import redirect, url_for, request
 
-main = Blueprint("main", __name__)
+from flask_login import login_user
+
+from app.main import main
+from app.main.forms import UploadForm, AnswerForm, LoginForm
+from app.models import User
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -42,3 +46,15 @@ def index():
         correct=correct,
         upload=upload,
     )
+
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.verify_password(form.password.data):
+            return redirect(url_for('main.login', **request.args))
+        login_user(user, form.remember_me.data)
+        return redirect(request.args.get('next') or url_for('main.index'))
+    return render_template('login.html', form=form)
