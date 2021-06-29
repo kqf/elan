74310c9cb877afe2651553a2e299c7e2aa9cada3
kqf <q.dev.null@gmail.com>
2021-06-29 19:00:23 +0200

    Add a dummy user (#30)
    * Add a dummy user at the start of the app
    
    * Fix the db crash
    
    * Create all tables at the start
    
    * Update the .gitignore
    
    * Fix the configuration

--- app/run.py
+++ app/run.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_session import Session
@@ -15,15 +16,21 @@ def build_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'SECRET_KEY'
     app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
+        os.path.dirname(__file__), '../data-dev.sqlite3')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     bootstrap.init_app(app)
     session.init_app(app)
+    db.init_app(app)
     app.register_blueprint(main_bp)
     return app
 
 
 def main():
     app = build_app()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
 
