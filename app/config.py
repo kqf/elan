cebb8c2c97e6794c09f3000768c1d0695542402e
Sasha <q.dev.null@gmail.com>
2021-06-30 07:53:00 +0200

    Add a separate config file

---
+++ app/config.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+
+
+from app.main.routes import main as main_bp
+
+bootstrap = Bootstrap()
+session = Session()
+db = SQLAlchemy()
+
+
+def build_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'SECRET_KEY'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
+        os.path.dirname(__file__), '../data-dev.sqlite3')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+    bootstrap.init_app(app)
+    session.init_app(app)
+    db.init_app(app)
+    app.register_blueprint(main_bp)
+    return app
