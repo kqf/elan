f182bc74a3bb4570d1fd471e3a32ec152aae7b35
kqf <q.dev.null@gmail.com>
2021-06-30 08:07:54 +0200

    Add a dummy user at start (#32)

--- app/run.py
+++ app/run.py
@@ -1,11 +1,14 @@
 from app import db
 from app.config import build_app
+from app.models import User
 
 
 def main():
     app = build_app()
     with app.app_context():
         db.create_all()
+        if User.query.filter_by(username='bob').first() is None:
+            User.register('bob', 'lol')
     app.run(debug=True)
 
 
