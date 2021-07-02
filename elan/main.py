ae7b330d5f95a56241d7f3f3c4c75e8423f9a385
kqf <q.dev.null@gmail.com>
2021-07-02 17:11:58 +0200

    Remove unused UI (#36)
    * Remove the ui definition
    
    * Remove the word checker from the console script

--- elan/main.py
+++ elan/main.py
@@ -4,15 +4,9 @@ from elan.words import WordsChecker
 
 @click.command()
 @click.option("--infile", type=click.Path(exists=True))
-@click.option("--mode", type=click.Choice(["gui", "cli"]), default="cli")
 @click.option("--lesson", type=str)
 @click.option("--objective", type=str)
 def main(infile, mode, lesson, objective):
-    if mode == "gui":
-        from ui import gui
-        gui(infile, lesson, objective)
-        return
-
     w = WordsChecker(infile, lesson)
     w.check(objective)
 
