d031da46577b469de4f75d1182b43a0562744d91
kqf <q.dev.null@gmail.com>
2021-07-04 11:30:49 +0200

    Add the tests for the login (#39)
    * Add the tests for the login
    
    * Split context fixture

---
+++ tests/test_login.py
@@ -0,0 +1,40 @@
+
+import pytest
+
+from app import db
+from app.config import build_app
+from app.models import User
+
+
+@pytest.fixture
+def app():
+    app = build_app()
+    app_ctx = app.app_context()
+    app_ctx.push()
+    db.create_all()
+    User.register('john', 'cat')
+    yield app
+    db.drop_all()
+    app_ctx.pop()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client(use_cookies=True)
+
+
+def test_login(app, client):
+    r = client.get('/login')
+    assert r.status_code == 200
+    assert '<h1>Login</h1>' in r.get_data(as_text=True)
+
+    r = client.post(
+        '/login',
+        data={
+            'username': 'john',
+            'password': 'cat'
+        },
+        follow_redirects=True
+    )
+    assert r.status_code == 200
+    assert '<h1>Login</h1>' in r.get_data(as_text=True)
