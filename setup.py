dc29841d55a11631f81ef64bbeb9342394511bb3
Sasha <q.dev.null@gmail.com>
2021-07-07 17:29:56 +0200

    Add the console script

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name="elan",
     version="0.0.1",
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'elan=app.run:main',
+        ],
+    },
 )
