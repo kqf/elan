e821b5e46b6b263bdb78c5b2d2aece46e5f51fb9
Sasha <q.dev.null@gmail.com>
2021-08-10 06:50:45 +0200

    Add the reading logic

--- app/run.py
+++ app/run.py
@@ -1,14 +1,21 @@
 import click
-
 from app import db
 from app.config import build_app
 from app.models import User
 
 
 @click.command()
-@click.option("--infile", type=click.Path(exists=True))
-def main(infile):
+@click.option("--filename", type=click.Path(exists=True))
+def main(filename):
+    tasks = []
+    with open(filename) as f:
+        for line in f.readlines():
+            original, expected = line.decode("utf-8").split("|")
+            tasks.append((original.strip(), expected.strip()))
+
     app = build_app()
+    app.session['tasks'] = tasks
+
     with app.app_context():
         db.create_all()
         if User.query.filter_by(username='bob').first() is None:
