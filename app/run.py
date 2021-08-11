d845b729c1e9585dd06c8f5358624666eac878aa
kqf <q.dev.null@gmail.com>
2021-08-11 07:06:38 +0200

    Remove cli code (#50)
    * Import procfile
    
    * Fix the path to the build function
    
    * Remvoe the argument itself
    
    * Fix the static checks

--- app/run.py
+++ app/run.py
@@ -1,21 +1,10 @@
-import click
 from app import db
 from app.config import build_app
 from app.models import User
 
 
-@click.command()
-@click.option("--filename", type=click.Path(exists=True))
-def main(filename):
-    tasks = []
-    with open(filename) as f:
-        for line in f.readlines():
-            original, expected = line.decode("utf-8").split("|")
-            tasks.append((original.strip(), expected.strip()))
-
+def main():
     app = build_app()
-    app.session['tasks'] = tasks
-
     with app.app_context():
         db.create_all()
         if User.query.filter_by(username='bob').first() is None:
