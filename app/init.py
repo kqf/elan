1400d34f3f6068f0672803671e204edc37a7d6ba
kqf <q.dev.null@gmail.com>
2021-08-12 11:45:00 +0200

    Add the init method (#52)
    * Add gunicorn configuration script
    
    * Fix the procfile

---
+++ app/init.py
@@ -0,0 +1,13 @@
+from app import db
+from app.models import User
+
+
+preload_app = True
+
+
+def on_starting(server):
+    app = server.app.callable
+    with app.app_context():
+        db.create_all()
+        if User.query.filter_by(username='bob').first() is None:
+            User.register('bob', 'lol')
