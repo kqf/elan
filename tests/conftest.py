779126cb1b13443cf4e31b703a82f0de15b34a28
Sasha <you@example.com>
2022-07-26 09:56:00 +0100

    Add the missing conftest

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+
+import pytest
+import flask_login
+
+from app import db
+from app.config import build_app
+from app.models import User
+
+
+@pytest.fixture
+def app():
+    app = build_app()
+    app_ctx = app.app_context()
+    app_ctx.push()
+    db.create_all()
+    User.register('john', 'cat')
+    with app.test_request_context():
+        yield app
+    db.drop_all()
+    app_ctx.pop()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client(use_cookies=True)
