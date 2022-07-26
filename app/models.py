1be5feba717d68b9d25ad4e1c3366491dd1877ed
kqf <q.dev.null@gmail.com>
2022-07-26 11:11:22 +0100

    Add user routes (#56)
    * Add more routes
    
    * Add the basic routes
    
    * Add advanced user methods
    
    * Add create method
    
    * Fix the tests for the user model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,17 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+from __future__ import annotations
+from crypt import methods
 
+from flask import Response, jsonify, request
+from flask_login import UserMixin
+from traitlets import ValidateHandler
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import db, lm
+from app.main import main
+
+
+class ValidationError(ValueError):
+    ...
 
 
 class User(UserMixin, db.Model):
@@ -18,16 +27,65 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     @staticmethod
-    def register(username, password):
+    def register(username, password) -> User:
         user = User(username=username)
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
+        return user
 
     def __repr__(self):
         return '<User {0}>'.format(self.username)
 
+    def url(self) -> str:
+        return "template"
+
+    def export(self) -> dict[str, str]:
+        return {
+            "name": self.username,
+            "url": self.url(),
+        }
+
+    def fromdict(self, data: dict[str, str]) -> User:
+        if "name" not in data:
+            raise ValidationError(f"Input has no 'name' field. Got {data}")
+
+        if "password" not in data:
+            raise ValidationError(f"Input has no 'password' field. Got {data}")
+
+        self.username = data["name"]
+        self.set_password(data["password"])
+        return self
+
 
 @lm.user_loader
-def load_user(id):
+def load_user(id):  # sourcery skip: instance-method-first-arg-name
     return User.query.get(int(id))
+
+
+@main.route("/users/", methods=["GET"])
+def users() -> Response:
+    return jsonify({"users": [u.url() for u in User.query.all()]})
+
+
+@main.route("/users/<int:id>", methods=["GET"])
+def user(id: int) -> Response:
+    return jsonify(User.query.get_or_404(id).export())
+
+
+@main.route("/users/", methods=["POST"])
+def create() -> tuple[Response, int, dict[str, str]]:
+    user = User()
+    user.fromdict(request.json)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({}), 201, {"Location": user.url()}
+
+
+@main.route("/users/<int:id>", methods=["PUT"])
+def update(id: int) -> Response:
+    user = User.query.get_or_404(id)
+    user.fromdict(request.json)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({})
--- tests/test_models.py
+++ tests/test_models.py
@@ -15,3 +15,41 @@ def test_registration(client):
     assert u is not None
     assert u.verify_password('lol')
     assert not u.verify_password('rolf')
+
+
+def test_retrieves_users(client):
+    response = client.get("/users", follow_redirects=True)
+    assert response.data == b'{"users":["template"]}\n'
+    assert response.status_code == 200
+
+def test_retrieves_a_user(client):
+    response = client.get("/users/1", follow_redirects=True)
+    assert response.data == b'{"name":"john","url":"template"}\n'
+    assert response.status_code == 200
+
+def test_creates_a_user(client):
+    response = client.post("/users/", json={
+        "name": "Bob",
+        "password": "Lol"
+    })
+    assert response.data == b'{}\n'
+    assert response.status_code == 201
+
+    # Check it does have an effect
+    user = User.query.get(2)
+    assert user.username == "Bob"
+    assert user.verify_password('Lol')
+
+
+def test_updates_a_user(client):
+    response = client.put("/users/1", json={
+        "name": "Bob",
+        "password": "Lol"
+    })
+    assert response.data == b'{}\n'
+    assert response.status_code == 200
+
+    # Check it does have an effect
+    user = User.query.get(1)
+    assert user.username == "Bob"
+    assert user.verify_password('Lol')
