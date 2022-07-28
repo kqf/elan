f9100405dc76650b1f4ecff41fabba99365f24a2
Sasha <q.dev.null@gmail.com>
2022-07-28 10:44:27 +0100

    Remove the long lines

--- tests/test_models.py
+++ tests/test_models.py
@@ -24,7 +24,8 @@ def test_retrieves_users(client):
 
 def test_retrieves_a_user(client):
     response = client.get("/users/1", follow_redirects=True)
-    assert response.data == b'{"name":"john","url":"http://localhost/users/1"}\n'
+    expected = b'{"name":"john","url":"http://localhost/users/1"}\n'
+    assert response.data == expected
     assert response.status_code == 200
 
 
