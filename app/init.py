a74af442120ce1da99b3e4f225dd91fd3bd71e80
kqf <q.dev.null@gmail.com>
2022-07-29 07:14:56 +0100

    Apply pre commit rules (#63)
    * Fix the formatting
    
    * Fix the CI

--- app/init.py
+++ app/init.py
@@ -1,7 +1,6 @@
 from app import db
 from app.models import User
 
-
 preload_app = True
 
 
@@ -9,5 +8,5 @@ def on_starting(server):
     app = server.app.callable
     with app.app_context():
         db.create_all()
-        if User.query.filter_by(username='bob').first() is None:
-            User.register('bob', 'lol')
+        if User.query.filter_by(username="bob").first() is None:
+            User.register("bob", "lol")
