ed965ae0c5d840e510b839215ee8d54b0954c2bd
kqf <q.dev.null@gmail.com>
2022-08-01 06:55:57 +0100

    Correct the test data (#69)
    * Fix creates a pair
    
    * Fix the second test
    
    * Update the create operation
    
    * Fix the rest of the tests

--- tests/models/test_pair.py
+++ tests/models/test_pair.py
@@ -23,39 +23,39 @@ def example(client):
     yield data
 
 
-def test_retrieves_a_user(client, example):
+@pytest.fixture
+def new_example():
+    return {
+        "iffield": "une oiseau",
+        "offield": "a bird",
+        "url": "http://localhost/pairs/2",
+    }
+
+
+def test_retrieves_a_pair(client, example):
     response = client.get("/pairs/1", follow_redirects=True)
     # expected = b'{"name":"john","url":"http://localhost/pairs/1"}\n'
     assert response.data == to_response(example)
     assert response.status_code == 200
 
 
-@pytest.mark.skip
-def test_retrieves_users(client, example):
-    response = client.get("/pairs", follow_redirects=True)
-    assert response.data == b'{"pairs":["http://localhost/pairs/1"]}\n'
-    assert response.status_code == 200
-
-
-@pytest.mark.skip
-def test_creates_a_user(client):
-    response = client.post("/pairs/", json={"name": "Bob", "password": "Lol"})
+def test_creates_a_pair(client, example, new_example):
+    response = client.post("/pairs/", json=new_example)
     assert response.data == b"{}\n"
     assert response.status_code == 201
 
     # Check it does have an effect
-    user = Pair.query.get(2)
-    assert user.username == "Bob"
-    assert user.verify_password("Lol")
+    pair: Pair = Pair.query.get(2)
+    assert pair.iffield == new_example["iffield"]
+    assert pair.offield == new_example["offield"]
 
 
-@pytest.mark.skip
-def test_updates_a_user(client):
-    response = client.put("/pairs/1", json={"name": "Bob", "password": "Lol"})
+def test_updates_a_pair(client, example, new_example):
+    response = client.put("/pairs/1", json=new_example)
     assert response.data == b"{}\n"
     assert response.status_code == 200
 
     # Check it does have an effect
-    user = Pair.query.get(1)
-    assert user.username == "Bob"
-    assert user.verify_password("Lol")
+    pair = Pair.query.get(1)
+    assert pair.iffield == new_example["iffield"]
+    assert pair.offield == new_example["offield"]
