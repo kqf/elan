2d14642c1097a8cf8e6e83f6efeb39fcb62039dd
kqf <q.dev.null@gmail.com>
2022-08-01 07:09:32 +0100

    Add autovalidation (#70)
    * Add automatic field checking
    
    * Apply automatic error generator
    
    * Fix pair model
    
    * Simplify the users

--- app/models/exception.py
+++ app/models/exception.py
@@ -1,2 +1,24 @@
+import functools
+from typing import Any
+
+from flask import request
+
+
 class ValidationError(ValueError):
     ...
+
+
+def requires_fields(*fields):
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            data: dict[str, str] | Any = request.json
+            if missing := ",".join(a for a in fields if a not in data):
+                raise ValidationError(
+                    f"Missing fields detected {missing}. Got {data}"
+                )
+            return f(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
