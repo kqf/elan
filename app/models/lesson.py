4886d38bad521012c1a00b224a4e04fca66958a0
kqf <q.dev.null@gmail.com>
2022-08-05 18:52:10 +0100

    Add lesson (#71)
    * Add the basic lesson
    
    * Add delete orphans

---
+++ app/models/lesson.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from flask import url_for
+
+from app import db
+
+
+class Lesson(db.Model):
+    __tablename__ = "lessons"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    pairs = db.relationship(
+        "Pair", backref="Lesson", lazy="dynamic", cascade="all, delete-orphan"
+    )
+
+    def url(self) -> str:
+        return url_for("main.lesson", id=self.id, _external=True)
+
+    def export(self) -> dict[str, str]:
+        return {
+            "title": self.title,
+            "pairs": "pairs/url",
+            "url": self.url(),
+        }
