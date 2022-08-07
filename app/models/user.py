e919d3331324c42763fc5b2f045d3bde6a6549ca
Sasha <q.dev.null@gmail.com>
2022-08-07 10:23:13 +0100

    Add the table definition

--- app/models/user.py
+++ app/models/user.py
@@ -16,6 +16,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), index=True, unique=True)
     password_hash = db.Column(db.String(64))
+    orders = db.relationship("Lesson", backref="user", lazy="dynamic")
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
