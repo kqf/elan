1bbc6cfd2b677a1733d0d77069217ec57804fe24
Sasha <q.dev.null@gmail.com>
2022-08-07 10:15:11 +0100

    Add the route

--- app/models/lesson.py
+++ app/models/lesson.py
@@ -51,3 +51,9 @@ def build_lesson() -> tuple[Response, int, dict[str, str]]:
 @main.route("/lessons/<int:id>", methods=["GET"])
 def lesson(id) -> Response:
     return jsonify(Lesson.query.get_or_404(id).export())
+
+
+@main.route("/lessons/<int:id>/data", methods=["GET"])
+def lesson_data(id: int) -> Response:
+    lesson = Lesson.query.get_or_404(id)
+    return jsonify([pair.url() for pair in lesson.pairs.all()])
