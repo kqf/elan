3954864b390f0f906a90431aa9a4a79035ee0ecd
Sasha <q.dev.null@gmail.com>
2022-08-16 15:59:44 +0100

    Add the necessary column

--- app/models/user.py
+++ app/models/user.py
@@ -26,6 +26,7 @@ class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(64))
     tokens = db.relationship("Token", back_populates="user", lazy="noload")
     lessons = db.relationship("Lesson", backref="user", lazy="dynamic")
