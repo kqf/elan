2dd2e6c2c8eaca334c07d602a2ae24a4eea26fb3
kqf <q.dev.null@gmail.com>
2022-08-22 14:14:17 +0100

    Re introduce auth (#118)
    * Fix the auth objects
    
    * Fix the routes paths
    
    * Move the imports around
    
    * MoAdd auth routes
    
    * Fix the users

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 lm = LoginManager()
+basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
+
 lm.login_view = "main.login"
