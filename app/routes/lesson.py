0caa2b5040de5dc559c6b5dc6a77235150fe8223
Sasha <q.dev.null@gmail.com>
2022-08-22 14:29:56 +0100

    Add the lesson routes

---
+++ app/routes/lesson.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from flask import Response, jsonify
+
+from app.main import main
+from app.models.lesson import Lesson
+
+
+@main.route("/lessons/<int:id>", methods=["GET"])
+def lesson(id) -> Response:
+    return jsonify(Lesson.query.get_or_404(id).export())
+
+
+@main.route("/lessons/<int:id>/data", methods=["GET"])
+def lesson_data(id: int) -> Response:
+    lesson = Lesson.query.get_or_404(id)
+    return jsonify([pair.url() for pair in lesson.pairs.all()])
