0551dfb4bcb9234b43bb8d4de58763b50d8b87d4
Sasha <q.dev.null@gmail.com>
2022-08-23 15:53:53 +0100

    Add lessons themeselves

--- app/models/models.py
+++ app/models/models.py
@@ -60,6 +60,26 @@ class User(UserMixin, db.Model):
         return url_for("main.user", id=self.id, _external=True)
 
 
+class Lesson(db.Model):
+    __tablename__ = "lessons"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), index=True)
+    pairs = db.relationship(
+        "Pair", backref="Lesson", lazy="dynamic", cascade="all, delete-orphan"
+    )
+
+    def url(self) -> str:
+        return url_for("main.lesson", id=self.id, _external=True)
+
+    def export(self) -> dict[str, str]:
+        return {
+            "title": self.title,
+            "pairs": url_for("main.lesson_data", id=self.id, _external=True),
+            "url": self.url(),
+        }
+
+
 class Pair(db.Model):
     __tablename__ = "pairs"
     id = db.Column(db.Integer, primary_key=True)
