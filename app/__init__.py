3852fcdbb1028b6ee001a47dfbb43465907a8a0b
Sasha <q.dev.null@gmail.com>
2022-08-28 13:10:39 +0100

    Add hte missing blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
+from flask import Blueprint
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 basic_auth = HTTPBasicAuth()
 token_auth = HTTPTokenAuth()
+main = Blueprint("main", __name__)
