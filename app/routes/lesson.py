f526258ce528b71736de2db504701b8bb55a9194
kqf <q.dev.null@gmail.com>
2022-08-30 10:12:19 +0100

    Fix lessons routes (#136)
    * Add the lessons blueprint
    
    * Register lesson blueprint

--- app/routes/lesson.py
+++ app/routes/lesson.py
@@ -1,17 +1,18 @@
 from __future__ import annotations
 
-from flask import Response, jsonify
+from flask import Blueprint, Response, jsonify
 
-from app import main
 from app.models.models import Lesson
 
+lessons = Blueprint("lessons", __name__)
 
-@main.route("/lessons/<int:id>", methods=["GET"])
+
+@lessons.route("/lessons/<int:id>", methods=["GET"])
 def lesson(id) -> Response:
     return jsonify(Lesson.query.get_or_404(id).export())
 
 
-@main.route("/lessons/<int:id>/data", methods=["GET"])
+@lessons.route("/lessons/<int:id>/data", methods=["GET"])
 def lesson_data(id: int) -> Response:
     lesson = Lesson.query.get_or_404(id)
     return jsonify([pair.url() for pair in lesson.pairs.all()])
