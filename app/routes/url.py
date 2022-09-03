e1de8c9bd8bdb8584d356c82a1918f16973ffaf0
Sasha <q.dev.null@gmail.com>
2022-09-03 16:38:27 +0100

    Fix the url method

--- app/routes/url.py
+++ app/routes/url.py
@@ -1,7 +1,10 @@
+from operator import attrgetter
+
 from flask import url_for
 
 from app import db
 
 
-def url(route: str, model: db.Model) -> str:
-    return url_for(route, id=model.id, _external=True)
+def url(route: str, model: db.Model, column="id") -> str:
+    id_for = attrgetter(column)
+    return url_for(route, id=id_for(model), _external=True)
