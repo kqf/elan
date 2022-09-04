e51c7a59193934209ec06b95c4b9c23db169711c
kqf <q.dev.null@gmail.com>
2022-09-04 05:46:13 +0100

    Fix lesson url method (#147)
    * Fix the exception module
    
    * Fix the imports
    
    * Fix the routes
    
    * Update the url method
    
    * Fix the url method
    
    * Convert the lesson
    
    * Fix the url for lessosn

--- app/routes/users.py
+++ app/routes/users.py
@@ -10,6 +10,7 @@ from app import db, token_auth
 from app.models.models import Lesson, Pair
 from app.routes.exception import requires_fields
 
+
 users = Blueprint("users", __name__)
 
 
@@ -48,7 +49,11 @@ def update(id: int) -> Response:
 def users_lessons(id: int) -> Response:
     user = users_.User.query.get_or_404(id)
     return jsonify(
-        {"lessons": [lesson.url() for lesson in user.lessons.all()]}
+        {
+            "lessons": [
+                url("lessons.lesson", lesson) for lesson in user.lessons.all()
+            ]
+        }
     )
 
 
