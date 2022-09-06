41e88244059b9cf0a890fe3f10cab2bcd9b4c5cc
Sasha <q.dev.null@gmail.com>
2022-09-06 09:34:34 +0200

    Fix the base cases

--- tests/routes/test_pair.py
+++ tests/routes/test_pair.py
@@ -52,7 +52,7 @@ def test_creates_a_pair(client, example, new_example):
 
 def test_updates_a_pair(client, example, new_example):
     response = client.put("/pairs/1", json=new_example)
-    assert response.json == "{}\n"
+    assert response.json == {}
     assert response.status_code == 200
 
     # Check it does have an effect
