e7cec8e6d8d0e833cd3ae4fa8adf80a8511e9faa
Sasha <q.dev.null@gmail.com>
2022-09-07 07:35:18 +0200

    Compare against json

--- tests/routes/test_pair.py
+++ tests/routes/test_pair.py
@@ -1,5 +1,3 @@
-import json
-
 import pytest
 
 from app.models.models import Pair
@@ -30,7 +28,7 @@ def new_example():
 def test_retrieves_a_pair(client, example):
     response = client.get("/pairs/1", follow_redirects=True)
     # expected = b'{"name":"john","url":"http://localhost/pairs/1"}\n'
-    assert response.data == to_response(example)
+    assert response.data == example
     assert response.status_code == 200
 
 
