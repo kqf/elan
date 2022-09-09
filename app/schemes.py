7e4abfc025f603e1f37b824a1aca7b988fe1e4c7
kqf <q.dev.null@gmail.com>
2022-09-09 16:20:58 +0200

    Add schemes (#157)
    * Add the marshmallow requirements
    
    * Add the example schemas

---
+++ app/schemes.py
@@ -0,0 +1,17 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+from app.model import Lesson, User
+
+
+class UserSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        include_relationships = True
+        load_instance = True
+
+
+class LessonSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Lesson
+        include_fk = True
+        load_instance = True
