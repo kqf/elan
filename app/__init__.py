0092c86ebbec59ea9c63a8eac3817f9809c26889
Sasha <q.dev.null@gmail.com>
2022-09-10 19:07:05 +0200

    Use the plugin

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+ma = Marshmallow()
 basic_auth = HTTPBasicAuth()
 token_auth = HTTPTokenAuth()
