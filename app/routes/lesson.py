e879a28cc8b5f5318ed55436b0be80571a7497af
kqf <q.dev.null@gmail.com>
2022-09-18 15:54:40 +0200

    Validate lesson content (#181)
    * Fix the lesson
    
    * Fix the testcase
    
    * Fix the code style
    
    * Fix the schema
    
    * Clean the lessons

--- app/routes/lesson.py
+++ app/routes/lesson.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
 from apifairy import response
-from flask import Blueprint, Response, jsonify
+from flask import Blueprint, Response
 
 from app.models import Lesson
-from app.routes.url import url
-from app.schemes import LessonSchema
+from app.schemes import LessonSchema, PairSchema
+
 
 lessons = Blueprint("lessons", __name__)
 
 lesson_schema = LessonSchema()
+pairs_schema = PairSchema(many=True)
+
 
 
 @lessons.route("/lessons/<int:id>", methods=["GET"])
@@ -19,6 +21,7 @@ def lesson(id) -> Response:
 
 
 @lessons.route("/lessons/<int:id>/data", methods=["GET"])
+@response(pairs_schema)
 def lesson_data(id: int) -> Response:
     lesson = Lesson.query.get_or_404(id)
-    return jsonify([url("pairs.pair", pair) for pair in lesson.pairs.all()])
+    return lesson.pairs.all()
