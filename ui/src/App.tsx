091160eae104d930cbed772cdb5c50734c8fc6ca
Sasha <q.dev.null@gmail.com>
2022-10-04 09:06:01 +0200

    Use the API calls

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState(0)
+  useEffect(() => {
+    fetch('/users').then(res => {
+      console.log(res)
+      return res.json
+    }).then(data => {
+      setUsers(data.length)
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-            Hello world
-        </p>
-          Learn React
+        <p>Hello world</p>
+        <p> {users}</p>
       </header>
     </div>
   );
