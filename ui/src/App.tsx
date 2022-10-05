38d69cf0f9f424a11ad4c9c5b8569fa0ece05032
kqf <q.dev.null@gmail.com>
2022-10-05 19:21:09 +0200

    Add api call (#226)
    * Fix the interfaces
    
    * Fix the configuration
    
    * Remove the redundant information
    
    * Use the API calls

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,20 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState(0)
+  useEffect(() => {
+    fetch('/users').then(res => {
+      console.log(res)
+      return res.json
+    }).then(data => {
+      setUsers(data.length)
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-            Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello world</p>
+        <p> {users}</p>
       </header>
     </div>
   );
