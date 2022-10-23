c122852dd518819d84b7a4c11cb882c566796fc3
Sasha <q.dev.null@gmail.com>
2022-10-23 16:38:09 +0200

    Lift everything to a function

--- api/app/simple.py
+++ api/app/simple.py
@@ -10,26 +10,28 @@ from app.routes.lesson import lessons
 from app.routes.pair import pairs
 from app.routes.users import users
 
-app = Flask(__name__)
-cors = CORS(app)
-app.config["SECRET_KEY"] = "SECRET_KEY"
-app.config["SESSION_TYPE"] = "filesystem"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
-    os.path.dirname(__file__), "../data-dev.sqlite3"
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config["ACCESS_TOKEN_MINUTES"] = 90
-app.config["REFRESH_TOKEN_DAYS"] = 18
-db.init_app(app)
-ma.init_app(app)
-
-
-app.register_blueprint(lessons)
-app.register_blueprint(pairs)
-app.register_blueprint(auths)
-app.register_blueprint(users)
-
 
 @app.route("/test")
 def test() -> dict[str, str]:
     return {"mymessage": "Hello world"}
+
+
+def main():
+    app = Flask(__name__)
+    cors = CORS(app)
+    app.config["SECRET_KEY"] = "SECRET_KEY"
+    app.config["SESSION_TYPE"] = "filesystem"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+        os.path.dirname(__file__), "../data-dev.sqlite3"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    app.config["ACCESS_TOKEN_MINUTES"] = 90
+    app.config["REFRESH_TOKEN_DAYS"] = 18
+    db.init_app(app)
+    ma.init_app(app)
+
+    app.register_blueprint(lessons)
+    app.register_blueprint(pairs)
+    app.register_blueprint(auths)
+    app.register_blueprint(users)
+    return app
