af62022a8267277c42c8907558790925990adb23
kqf <q.dev.null@gmail.com>
2022-10-23 17:07:48 +0200

    Move simple to a func (#256)
    * add the missing databases
    
    * Fix the marshmallow
    
    * Fix the reimports
    
    * Remove broken init
    
    * Lift everything to a function
    
    * Add a test blueprint

--- api/app/simple.py
+++ api/app/simple.py
@@ -1,6 +1,6 @@
 import os  # isort: skip
 
-from flask import Flask  # isort: skip
+from flask import Blueprint, Flask  # isort: skip
 from flask_cors import CORS  # isort: skip
 from app import db  # isort: skip
 from app import ma  # isort: skip
@@ -10,26 +10,32 @@ from app.routes.lesson import lessons
 from app.routes.pair import pairs
 from app.routes.users import users
 
-app = Flask(__name__)
-cors = CORS(app)
-app.config["SECRET_KEY"] = "SECRET_KEY"
-app.config["SESSION_TYPE"] = "filesystem"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
-    os.path.dirname(__file__), "../data-dev.sqlite3"
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config["ACCESS_TOKEN_MINUTES"] = 90
-app.config["REFRESH_TOKEN_DAYS"] = 18
-db.init_app(app)
-ma.init_app(app)
+
+simple = Blueprint("simple", __name__)
 
 
-app.register_blueprint(lessons)
-app.register_blueprint(pairs)
-app.register_blueprint(auths)
-app.register_blueprint(users)
-
-
-@app.route("/test")
+@simple.route("/test")
 def test() -> dict[str, str]:
     return {"mymessage": "Hello world"}
+
+
+def main():
+    app = Flask(__name__)
+    cors = CORS(app)
+    app.config["SECRET_KEY"] = "SECRET_KEY"
+    app.config["SESSION_TYPE"] = "filesystem"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+        os.path.dirname(__file__), "../data-dev.sqlite3"
+    )
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    app.config["ACCESS_TOKEN_MINUTES"] = 90
+    app.config["REFRESH_TOKEN_DAYS"] = 18
+    db.init_app(app)
+    ma.init_app(app)
+
+    app.register_blueprint(lessons)
+    app.register_blueprint(pairs)
+    app.register_blueprint(auths)
+    app.register_blueprint(users)
+    app.register_blueprint(simple)
+    return app
