b70f57e7dcab41e06fa9a983358a1084427a7dfc
kqf <q.dev.null@gmail.com>
2022-11-02 10:33:24 +0100

    Check private routes (#285)
    * Add the code to test private routes
    
    * Fix the varnames
    
    * Check private routes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -21,6 +21,17 @@ async function click() {
 
   // @ts-ignore
   console.log("Fetched the token ->", body.token);
+  const userResponse  = await fetch('http://localhost:5050/users', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('accessToken'),
+    },
+    credentials: 'omit',
+  });
+
+  var ubody = await userResponse.json();
+  console.log(ubody)
 }
 
 function App() {
