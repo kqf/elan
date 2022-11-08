8630f3275b192b7674d1a02dc9cd4526b6d39155
Sasha <q.dev.null@gmail.com>
2022-11-08 07:05:56 +0100

    Clean the formatting

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -37,7 +37,6 @@ async function updateUsers() {
 
 function App() {
   const [users, setUsers] = useState(0)
-  authentificate()
 
   useEffect(() => {
     fetch('/test').then(res => {
@@ -52,9 +51,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Hello world</p>
-        <button onClick={updateUsers}>
+          <button onClick={updateUsers}>
               Here is the list of users {users}
-            </button>
+          </button>
         </header>
     </div>
   );
