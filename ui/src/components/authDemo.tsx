6799376dfcb511edfad3ce536ecff56c934c302c
Sasha <q.dev.null@gmail.com>
2022-11-16 07:52:35 +0100

    Add the error message

--- ui/src/components/authDemo.tsx
+++ ui/src/components/authDemo.tsx
@@ -54,6 +54,7 @@ function StatusWidget(props: {message: string, users: Array<User>}) {
               Get the list of users
           </button>
           <div>
+            {users.length === 0 && <p> No users exist</p>}
             <ul>
                {users.map(user => <li key={user.id.toString()}>{user.username}</li>)}
             </ul>
