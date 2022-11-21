08e152ed2cd553efcd4c0880a5a003357bac5c57
Sasha <q.dev.null@gmail.com>
2022-11-21 08:09:56 +0100

    Fix the protected button

--- ui/src/components/authDemo.tsx
+++ ui/src/components/authDemo.tsx
@@ -41,18 +41,25 @@ interface User {
 
 function StatusWidget(props: {message: string, users: Array<User>}) {
   const [users, setUsers] = useState(props.users);
+  const [logged, checkLogged] = useState(false);
 
+  console.log(logged)
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello world</p>
           Server response:
           {props.message}
-          <button className="btn btn-secondary btn-sm" onClick={authentificate}>
+          <button className="btn btn-secondary btn-sm"
+                  onClick={() => {
+                    authentificate().then(
+                      () => checkLogged(localStorage.getItem("accessToken") !== null)
+                  )}}
+          >
             Generate the token
           </button>
           <button className="btn btn-secondary btn-sm"
-                  disabled={localStorage.getItem('accessToken') == null}
+                  disabled={!logged}
                   onClick={() => {updateUsers().then(setUsers)}}
           >
               Get the list of users
