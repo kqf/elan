af5fc1676646956f79e8a9bcd8073fbefa26f1c2
Sasha <q.dev.null@gmail.com>
2022-11-27 08:06:51 +0100

    Add the app menu

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,9 +2,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import './App.css';
 import AuthDemo from './components/authDemo';
+import AppMenu from './components/menuComponent';
 
 function App() {
-  return <AuthDemo />
+  return <div>
+      <AppMenu />
+      <AuthDemo />
+    </div>
 }
 
 export default App;
