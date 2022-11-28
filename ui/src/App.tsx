daf58261cfda9009afe7210610cf3aeb422c9661
kqf <q.dev.null@gmail.com>
2022-11-28 08:28:40 +0100

    Add anew component to app (#346)
    * Fix the response
    
    * Fix the endpoint called
    
    * Cleanup the code
    
    * Refactor the widget
    
    * Differetntiate the calls
    
    * Fix the badge widget
    
    * Remove the props
    
    * Remove the unnecessary props
    
    * Fix the type
    
    * Fix the auth widget
    
    * Add the new component
    
    * Add the app menu
    
    * Center the divs

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,9 +2,24 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import './App.css';
 import AuthDemo from './components/authDemo';
+import AppMenu from './components/menuComponent';
 
 function App() {
-  return <AuthDemo />
+  // return <div>
+  //     <AppMenu />
+  //     <AuthDemo />
+  //   </div>
+
+  return (
+        <div className="row">
+            <div className="col-xs-4">
+              <AppMenu />
+            </div>
+            <div className="col-xs-6">
+                <AuthDemo />
+            </div>
+      </div>
+  );
 }
 
 export default App;
