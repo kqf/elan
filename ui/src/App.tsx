45c3cf7c5811176e8b27f3a8e109b3e115f2a9ad
kqf <q.dev.null@gmail.com>
2022-11-30 08:04:38 +0100

    Simplify the default styling (#348)
    * Fix the response
    
    * Fix the endpoint called
    
    * Cleanup the code
    
    * Refactor the widget
    
    * Differetntiate the calls
    
    * Fix the badge widget
    
    * Remove the props
    
    * Remove the unnecessary props
    
    * Fix the type
    
    * Fix the auth widget
    
    * Add the new component
    
    * Add the app menu
    
    * Center the divs
    
    * Remove the default styling
    
    * Simplify the default styling

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,26 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-// import './App.css';
 import AuthDemo from './components/authDemo';
 import AppMenu from './components/menuComponent';
 
 function App() {
-  // return <div>
-  //     <AppMenu />
-  //     <AuthDemo />
-  //   </div>
-
   return (
     <div className="container">
-        <div className="row">
-            <div className="col-xs-6">
-              <AppMenu />
-            </div>
-            <div className="col-xs-6">
-                <AuthDemo />
-            </div>
+      <div className="row">
+        <div className="col-xs-6">
+          <AppMenu />
+        </div>
+        <div className="col-xs-6">
+          <AuthDemo />
         </div>
       </div>
+    </div>
   );
 }
 
