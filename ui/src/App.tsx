96aa1cd0fc6964d23d6b53c353efd999b0382c94
kqf <q.dev.null@gmail.com>
2022-12-03 09:33:44 +0100

    Restore side by side (#349)
    * Fix the response
    
    * Fix the endpoint called
    
    * Cleanup the code
    
    * Refactor the widget
    
    * Differetntiate the calls
    
    * Fix the badge widget
    
    * Remove the props
    
    * Remove the unnecessary props
    
    * Fix the type
    
    * Fix the auth widget
    
    * Add the new component
    
    * Add the app menu
    
    * Center the divs
    
    * Remove the default styling
    
    * Simplify the default styling
    
    * Make the widges side by side

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
+import './App.css'
+
 import AuthDemo from './components/authDemo';
 import AppMenu from './components/menuComponent';
 
@@ -7,10 +9,10 @@ function App() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-xs-6">
+        <div className="col-xs-6 equal-height equal-width">
           <AppMenu />
         </div>
-        <div className="col-xs-6">
+        <div className="col-xs-6 equal-height equal-width">
           <AuthDemo />
         </div>
       </div>
