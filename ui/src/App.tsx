ed536051c39e14a57cd651b4f0f4115dfffe0b7d
Sasha <q.dev.null@gmail.com>
2022-12-03 09:32:33 +0100

    Make the widges side by side

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
+import './App.css'
 import AuthDemo from './components/authDemo';
 import AppMenu from './components/menuComponent';
 
@@ -7,10 +8,10 @@ function App() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-xs-6">
+        <div className="col-xs-6 equal-height equal-width">
           <AppMenu />
         </div>
-        <div className="col-xs-6">
+        <div className="col-xs-6 equal-height equal-width">
           <AuthDemo />
         </div>
       </div>
