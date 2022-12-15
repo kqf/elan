12c9c13416fb451533cdc1f494c07f7aeb7c8da7
kqf <q.dev.null@gmail.com>
2022-12-15 12:33:09 +0100

    Add state based counterws (#359)
    * Add the counters
    
    * Add the counter
    
    * Add the state
    
    * Add the counters

--- ui/src/components/coutners.tsx
+++ ui/src/components/coutners.tsx
@@ -2,20 +2,38 @@ import { useState } from "react";
 
 function Counter() {
   const [counts, updateCounts] = useState(0);
+  const increment = () => {
+    updateCounts(counts + 1);
+  };
   return (
-    <div
-      onClick={() => {
-        updateCounts(counts + 1);
-      }}
-    >
-      Counts: {counts}
+    <div onClick={increment}>
+      <span>{counts}</span>
+      <button className="btn btn-secondary btn-sm">Increment</button>
     </div>
   );
 }
+
 function Counters() {
+  const [counters, updateCounters] = useState([
+    {
+      id: 1,
+      value: 0,
+    },
+    {
+      id: 2,
+      value: 0,
+    },
+    {
+      id: 3,
+      value: 0,
+    },
+  ] as Array<{ id: number; value: number }>);
+
   return (
     <div>
-      <Counter />
+      {counters.map((c) => (
+        <Counter key={c.id} />
+      ))}
     </div>
   );
 }
