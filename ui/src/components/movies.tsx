de54e2a9eefe54c14984902fb9eaa5c5d87ae13a
kqf <q.dev.null@gmail.com>
2022-12-21 06:59:34 +0100

    Add movies components (#370)
    * Add the movie table
    
    * Render the movie component

---
+++ ui/src/components/movies.tsx
@@ -0,0 +1,24 @@
+function Movies() {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Genere</th>
+          <th>Stock</th>
+          <th>Rate</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>a</td>
+          <td>b</td>
+          <td>c</td>
+          <td>d</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default Movies;
