3c52fbfb53ab9510a3ad83597cfd47157e39332c
Sasha <q.dev.null@gmail.com>
2022-12-21 06:59:01 +0100

    Render the movie component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 
 import AuthDemo from "./components/authDemo";
 import AppMenu from "./components/menuComponent";
+import Movies from "./components/movies";
 import NavBar from "./components/navbar";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <div className="row">
         <div className="col-xs-6 equal-width">
           <AppMenu />
+          <Movies />
         </div>
         <div className="col-xs-6 equal-width">
           <AuthDemo />
