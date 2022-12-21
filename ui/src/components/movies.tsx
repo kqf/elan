69045b0a6baf28f8ca716477ffb1b17fbc188dad
kqf <q.dev.null@gmail.com>
2022-12-21 07:13:49 +0100

    Generate the fake data (#371)
    * Generate the fake data
    
    * Add the movie table

--- ui/src/components/movies.tsx
+++ ui/src/components/movies.tsx
@@ -1,4 +1,20 @@
+const movies = ["1", "2", "3", "4"].map((i) => {
+  return {
+    _id: i,
+    title: `Title ${i}`,
+    genre: { _id: i, name: `name ${i}` },
+    numberInStock: i,
+    dailyRentalRate: 1.5,
+    publishDate: "2020-01-01",
+  };
+});
+
+function getMovies() {
+  return movies;
+}
+
 function Movies() {
+  const movies = getMovies();
   return (
     <table className="table">
       <thead>
@@ -10,12 +26,16 @@ function Movies() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>a</td>
-          <td>b</td>
-          <td>c</td>
-          <td>d</td>
-        </tr>
+        {movies.map((movie) => {
+          return (
+            <tr>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
