0251a361ed04e7f29f8df916a3ed7c71cc58f964
Sasha <q.dev.null@gmail.com>
2022-12-26 09:39:14 +0100

    Map data automatically

--- ui/src/components/pagination.tsx
+++ ui/src/components/pagination.tsx
@@ -1,16 +1,23 @@
+import _ from "lodash";
+
 function Pagination(props: {
   itemCount: number;
   pageSize: number;
   onClick: any;
 }) {
+  const pagesCount = props.itemCount / props.pageSize;
+  const pages = _.range(1, pagesCount + 1);
+
   return (
     <nav aria-label="Pagination">
       <ul className="pagination">
-        <li className="page-item">
-          <a className="page-link" href="">
-            1
-          </a>
-        </li>
+        {pages.map((page) => (
+          <li key={page} className="page-item">
+            <a className="page-link" href="">
+              {page}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
