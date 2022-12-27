d71fd232ec3db699fcdd55678f39269149a5ae62
kqf <q.dev.null@gmail.com>
2022-12-27 08:00:46 +0100

    Implement pagination component (#382)
    * Add the pagination
    
    * Add a new library
    
    * Add type annotatins
    
    * Map data automatically
    
    * Reduce the page size

--- ui/src/components/pagination.tsx
+++ ui/src/components/pagination.tsx
@@ -1,9 +1,26 @@
+import _ from "lodash";
+
 function Pagination(props: {
   itemCount: number;
   pageSize: number;
   onClick: any;
 }) {
-  return <div></div>;
+  const pagesCount = props.itemCount / props.pageSize;
+  const pages = _.range(1, pagesCount + 1);
+
+  return (
+    <nav aria-label="Pagination">
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li key={page} className="page-item">
+            <a className="page-link" href="">
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default Pagination;
