fca65af38f30ab19808530fabd96356acf9df201
kqf <q.dev.null@gmail.com>
2022-12-30 10:55:04 +0100

    Fix pagination annotations (#389)
    * Add the generic function
    
    * Apply the pagination
    
    * Fix the broken annotations

--- ui/src/paginate.tsx
+++ ui/src/paginate.tsx
@@ -1,6 +1,6 @@
 import _ from "lodash";
 
-function paginate(items: any, pageNumber: number, pageSize: number) {
+function paginate<T>(items: Array<T>, pageNumber: number, pageSize: number) {
   const startIndex = (pageNumber - 1) * pageSize;
   return _(items).slice(startIndex).take(pageSize).value();
 }
