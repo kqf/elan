333a44d6c6fcbad0c6cf12eb894e59618a1fe2ce
kqf <q.dev.null@gmail.com>
2022-12-31 09:07:52 +0100

    Add list group component (#392)
    * Fix the typo
    
    * Add the listgroup component
    
    * Add the component

--- ui/src/components/movies.tsx
+++ ui/src/components/movies.tsx
@@ -1,7 +1,8 @@
 import _ from "lodash";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import paginate from "../paginate";
 import Like from "./like";
+import ListGroup from "./listGroup";
 import Pagination from "./pagination";
 
 interface Genre {
@@ -75,49 +76,54 @@ function Movies() {
   const paginated = paginate(state.movies, state.currentPage, state.pageSize);
 
   return (
-    <Fragment>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genere</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {paginated.map((movie) => {
-            return (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <Like liked={movie.liked} onClick={likeForMovie(movie)} />
-                </td>
-                <td>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={deleteMovie(movie)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <Pagination
-        itemCount={state.movies.length}
-        pageSize={state.pageSize}
-        currentPage={state.currentPage}
-        onClick={switchPage}
-      />
-    </Fragment>
+    <div className="row">
+      <div className="col-2">
+        <ListGroup />
+      </div>
+      <div className="col">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genere</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {paginated.map((movie) => {
+              return (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <Like liked={movie.liked} onClick={likeForMovie(movie)} />
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger btn-sm"
+                      onClick={deleteMovie(movie)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <Pagination
+          itemCount={state.movies.length}
+          pageSize={state.pageSize}
+          currentPage={state.currentPage}
+          onClick={switchPage}
+        />
+      </div>
+    </div>
   );
 }
 
