dca4e51d5d9466668c821c3328c1baad76f35c46
kqf <q.dev.null@gmail.com>
2023-01-05 08:18:00 +0100

    Fix the listimte warnings (#400)
    * Fix a typo
    
    * Add some meaningful genres
    
    * Add the meaningful list of genres
    
    * Fix the genres
    
    * Fix the listgroup
    
    * Reduce the number of genres
    
    * Fix the movies
    
    * Add two step filtering
    
    * Fix the method
    
    * Fix the data
    
    * Fix the warning

--- ui/src/components/listGroup.tsx
+++ ui/src/components/listGroup.tsx
@@ -1,8 +1,10 @@
 function ListGroup(props: { items: Array<String> }) {
   return (
     <ul className="list-gorup">
-      {props.items.map((item) => (
-        <li className="list-group-item"> {item} </li>
+      {props.items.map((item, i) => (
+        <li key={i} className="list-group-item">
+          {item}
+        </li>
       ))}
     </ul>
   );
