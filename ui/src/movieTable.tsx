246f1a013c1291241613d5f12155c5f9c0b8e8b0
Sasha <q.dev.null@gmail.com>
2023-01-11 07:57:30 +0100

    Add the missing table annotations

--- ui/src/movieTable.tsx
+++ ui/src/movieTable.tsx
@@ -17,8 +17,8 @@ export interface Movie {
 
 function MovieTable(props: {
   movies: Array<Movie>;
-  likeForMovie: any;
-  deleteMovie: any;
+  likeForMovie: (arg0: number) => () => void;
+  deleteMovie: (arg0: number) => () => void;
 }) {
   return (
     <table className="table">
