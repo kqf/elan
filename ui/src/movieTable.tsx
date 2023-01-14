70054a538a43e456f4bc0bf464d08d50859abf5c
kqf <q.dev.null@gmail.com>
2023-01-14 11:22:22 +0100

    Move sorting logic inside table (#424)
    * Add movies
    
    * Fix the movies component
    
    * Fix the table component

--- ui/src/movieTable.tsx
+++ ui/src/movieTable.tsx
@@ -15,20 +15,38 @@ export interface Movie {
   liked: boolean;
 }
 
+export interface SortingColumn {
+  column: String;
+  order: boolean | "asc" | "desc";
+}
+
 function MovieTable(props: {
   movies: Array<Movie>;
   likeForMovie: (arg0: Movie) => () => void;
   deleteMovie: (arg0: Movie) => () => void;
-  sortBy: (arg0: String) => () => void;
+  onSort: (column: SortingColumn) => void;
+  sortingBy: SortingColumn;
 }) {
+  const sortBy = (field: String) => () => {
+    let order = "asc" as "asc" | "desc";
+    if (props.sortingBy.column === field && props.sortingBy.order === "asc") {
+      order = "desc";
+    }
+    if (props.sortingBy.column === field && props.sortingBy.order === "desc") {
+      order = "asc";
+    }
+
+    props.onSort({ column: field, order: order });
+  };
+
   return (
     <table className="table">
       <thead>
         <tr>
-          <th onClick={props.sortBy("title")}>Title</th>
-          <th onClick={props.sortBy("genre.name")}>Genere</th>
-          <th onClick={props.sortBy("numberInStock")}>Stock</th>
-          <th onClick={props.sortBy("dailyRentalRate")}>Rate</th>
+          <th onClick={sortBy("title")}>Title</th>
+          <th onClick={sortBy("genre.name")}>Genere</th>
+          <th onClick={sortBy("numberInStock")}>Stock</th>
+          <th onClick={sortBy("dailyRentalRate")}>Rate</th>
           <th></th>
           <th></th>
         </tr>
