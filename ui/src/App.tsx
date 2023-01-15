66681e5f2ed915f43c34eed212d4971dc667c25b
Sasha <q.dev.null@gmail.com>
2023-01-15 08:00:29 +0100

    Add the basic routing

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 
 import AuthDemo from "./components/authDemo";
@@ -9,18 +10,20 @@ import NavBar from "./components/navbar";
 
 function App() {
   return (
-    <div className="container">
-      <NavBar />
-      <div className="row">
-        <div className="col-xs-6 equal-width">
-          <AppMenu />
-          <Movies />
-        </div>
-        <div className="col-xs-6 equal-width">
-          <AuthDemo />
+    <BrowserRouter>
+      <div className="container">
+        <NavBar />
+        <div className="row">
+          <div className="col-xs-6 equal-width">
+            <AppMenu />
+            <Movies />
+          </div>
+          <div className="col-xs-6 equal-width">
+            <AuthDemo />
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
