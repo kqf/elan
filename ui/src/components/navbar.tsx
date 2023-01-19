b5ef0e303619b381e4c1c0fecfe5bb4f2763f677
Sasha <q.dev.null@gmail.com>
2023-01-19 11:12:58 +0100

    Add the navbar

--- ui/src/components/navbar.tsx
+++ ui/src/components/navbar.tsx
@@ -1,11 +1,28 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
+      <NavLink className="navbar-brand" to="/">
         MyApp
-      </Link>
+      </NavLink>
+      <div className="navbar-nav">
+        <NavLink className="nav-item nav-link" to="/">
+          Home
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/calc">
+          Calculator
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/products">
+          Products
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/posts">
+          Posts
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/auth">
+          Auth
+        </NavLink>
+      </div>
     </nav>
   );
 }
