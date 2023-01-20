e19737b28571f6f9b6627ad312fe8f8e353f6ee8
kqf <q.dev.null@gmail.com>
2023-01-20 08:45:21 +0100

    Improve the navbar (#438)
    * Improve the navbar
    
    * Add the navbar

--- ui/src/components/navbar.tsx
+++ ui/src/components/navbar.tsx
@@ -1,7 +1,28 @@
+import { NavLink } from "react-router-dom";
+
 function NavBar() {
   return (
-    <nav className="navbar navbar-light bg-light">
-      <div>Navbar</div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <NavLink className="navbar-brand" to="/">
+        MyApp
+      </NavLink>
+      <div className="navbar-nav">
+        <NavLink className="nav-item nav-link" to="/">
+          Home
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/calc">
+          Calculator
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/products">
+          Products
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/posts">
+          Posts
+        </NavLink>
+        <NavLink className="nav-item nav-link" to="/auth">
+          Auth
+        </NavLink>
+      </div>
     </nav>
   );
 }
