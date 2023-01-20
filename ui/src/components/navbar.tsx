2a2a3e385571254e2c574b94381bba177a86c6af
kqf <q.dev.null@gmail.com>
2023-01-20 08:58:19 +0100

    Imporove the navbar (#440)
    * Add the span
    
    * Add the collapsable div

--- ui/src/components/navbar.tsx
+++ ui/src/components/navbar.tsx
@@ -6,22 +6,27 @@ function NavBar() {
       <NavLink className="navbar-brand" to="/">
         MyApp
       </NavLink>
-      <div className="navbar-nav">
-        <NavLink className="nav-item nav-link" to="/">
-          Home
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/calc">
-          Calculator
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/products">
-          Products
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/posts">
-          Posts
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/auth">
-          Auth
-        </NavLink>
+      <button className="navbar-toggler">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/calc">
+            Calculator
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/products">
+            Products
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/posts">
+            Posts
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/auth">
+            Auth
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
