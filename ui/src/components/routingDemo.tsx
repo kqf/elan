863fcf8bf979ec261dc7f8cc072b7ac1deaf888a
kqf <q.dev.null@gmail.com>
2023-01-24 08:06:34 +0100

    Add form itself (#447)
    * Add the default movies
    
    * Add the additional movies
    
    * Simplify
    
    * Fix the styling
    
    * Add the form submission itself

--- ui/src/components/routingDemo.tsx
+++ ui/src/components/routingDemo.tsx
@@ -132,9 +132,16 @@ type MovieParams = {
 
 function MovieComponent() {
   const params = useParams<MovieParams>();
+  const navigate = useNavigate();
   return (
     <div>
       <h1>This is movie {params.id}</h1>
+      <button
+        className="btn btn-primary"
+        onClick={() => navigate("/", { replace: true })}
+      >
+        Save
+      </button>
     </div>
   );
 }
