36e12d395ebf76aa655998bbed52a9c1a5c9a72f
Sasha <q.dev.null@gmail.com>
2023-01-24 08:46:35 +0100

    Add the simple form

--- ui/src/components/login.tsx
+++ ui/src/components/login.tsx
@@ -1,5 +1,19 @@
 function LoginForm() {
-  return <h1>Login</h1>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input id="username" className="form-control" type="text"></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input id="username" className="form-control" type="text"></input>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
