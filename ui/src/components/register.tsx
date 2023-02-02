3d3fa8f6f9370d83a7de9ac0b14e0803aee44a7c
kqf <q.dev.null@gmail.com>
2023-02-02 11:41:57 +0100

    Add the register page (#469)
    * Add the register page
    
    * Fix the navbar

---
+++ ui/src/components/register.tsx
@@ -0,0 +1,125 @@
+import { useState } from "react";
+
+interface FormElements extends HTMLFormControlsCollection {
+  username: HTMLInputElement;
+  password: HTMLInputElement;
+}
+
+interface UsernameFormElement extends HTMLFormElement {
+  readonly elements: FormElements;
+}
+
+function LoginField(props: {
+  name: string;
+  label: string;
+  error: string;
+  value: string;
+  onChange: (event: React.FormEvent<HTMLInputElement>) => void;
+}) {
+  return (
+    <div className="form-group">
+      <label htmlFor={props.name}>{props.label}</label>
+      <input
+        autoFocus
+        value={props.value}
+        onChange={props.onChange}
+        id={props.name}
+        className="form-control"
+        type="text"
+      ></input>
+      {props.error && <div className="alert alert-danger">{props.error}</div>}
+    </div>
+  );
+}
+
+function RegisterForm() {
+  const [state, setState] = useState({
+    account: {
+      username: "Defaut User",
+      password: "Default password",
+    },
+    errors: {
+      username: "",
+      password: "",
+    },
+  });
+
+  const validateProperty = (value: string) => {
+    if (value === "") return "Must not be empty";
+    if (value.length > 20) return "Too long, must be less than 20 characters";
+    return "";
+  };
+
+  const validate = (username: string, password: string) => {
+    return {
+      status: username.length > 0 && password.length > 0,
+      errors: {
+        username: username.length === 0 ? "Username can't be empty" : "",
+        password: password.length === 0 ? "Password can't be empty" : "",
+      },
+    };
+  };
+
+  const handleSubmit = (event: React.FormEvent<UsernameFormElement>) => {
+    event.preventDefault();
+
+    const { status, errors } = validate(
+      event.currentTarget.elements.username.value,
+      event.currentTarget.elements.password.value
+    );
+
+    if (!status) return;
+
+    setState({
+      ...state,
+      errors: errors,
+    });
+  };
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setState({
+      ...state,
+      account: {
+        ...state.account,
+        [event.currentTarget.id]: event.currentTarget.value,
+      },
+      errors: {
+        ...state.errors,
+        [event.currentTarget.id]: validateProperty(event.currentTarget.value),
+      },
+    });
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <LoginField
+          name="username"
+          label="Username"
+          value={state.account.username}
+          onChange={handleChange}
+          error={state.errors.username}
+        />
+        <LoginField
+          name="password"
+          label="Password"
+          value={state.account.password}
+          onChange={handleChange}
+          error={state.errors.password}
+        />
+        <button
+          disabled={
+            !validate(state.account.username, state.account.password).status
+          }
+          className="btn btn-primary"
+        >
+          Register
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterForm;
