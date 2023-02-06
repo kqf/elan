4fb78cefc340865ced9e4c2e22a5f57d2dc6ffd3
Sasha <q.dev.null@gmail.com>
2023-02-06 07:45:44 +0100

    Add the boilerplate code

--- ui/src/components/relogin.tsx
+++ ui/src/components/relogin.tsx
@@ -1,8 +1,41 @@
+import { useForm, Resolver } from "react-hook-form";
+
+type FormValues = {
+  firstName: string;
+  lastName: string;
+};
+
+const resolver: Resolver<FormValues> = async (values) => {
+  return {
+    values: values.firstName ? values : {},
+    errors: !values.firstName
+      ? {
+          firstName: {
+            type: "required",
+            message: "This is required.",
+          },
+        }
+      : {},
+  };
+};
+
 function ReloginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver });
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   return (
-    <div>
-      <h1>Re-login</h1>
-    </div>
+    <form onSubmit={onSubmit}>
+      <input {...register("firstName")} placeholder="Bill" />
+      {errors?.firstName && <p>{errors.firstName.message}</p>}
+
+      <input {...register("lastName")} placeholder="Luo" />
+
+      <input type="submit" />
+    </form>
   );
 }
 
