51781e5c43c5a20c88e8947c4500318ee4bbc229
kqf <q.dev.null@gmail.com>
2023-02-07 08:15:40 +0100

    Implement relogin (#481)
    * Add the boilerplate code
    
    * Add the relogin form
    
    * Fix a typo

--- ui/src/components/relogin.tsx
+++ ui/src/components/relogin.tsx
@@ -1,8 +1,63 @@
-function ReloginForm() {
+import {
+  useForm,
+  Resolver,
+  UseFormRegister,
+  FieldErrors,
+} from "react-hook-form";
+
+type FormValues = {
+  firstName: string;
+  lastName: string;
+};
+
+const resolver: Resolver<FormValues> = async (values) => {
+  return {
+    values: values.firstName ? values : {},
+    errors: !values.firstName
+      ? {
+          firstName: {
+            type: "required",
+            message: "This is required.",
+          },
+        }
+      : {},
+  };
+};
+
+function LoginField(props: {
+  name: "firstName" | "lastName";
+  placeholder: string;
+  register: UseFormRegister<FormValues>;
+  errors: FieldErrors<FormValues>;
+}) {
   return (
     <div>
-      <h1>Re-login</h1>
+      <input {...props.register(props.name)} placeholder={props.placeholder} />
+      {props.errors?.firstName && <p>{props.errors.firstName.message}</p>}
     </div>
+  );
+}
+
+function ReloginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver });
+  const onSubmit = handleSubmit((data) => console.log(data));
+
+  return (
+    <form onSubmit={onSubmit}>
+      <LoginField
+        name="firstName"
+        placeholder="Bob"
+        register={register}
+        errors={errors}
+      />
+      <input {...register("lastName")} placeholder="Luo" />
+
+      <input type="submit" />
+    </form>
   );
 }
 
