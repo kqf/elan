9b3bc2a6c84e90dd8a5a16fe5b5d9f1544d2c3c4
kqf <q.dev.null@gmail.com>
2023-02-08 08:32:43 +0100

    Fix the api ma sqla mismatch (#484)
    * Fix the relogin
    
    * Fix the labeling
    
    * Try to fix MA scheme
    
    * Fix the env versions
    
    * Fix everything again
    
    * Fix the schemes
    
    * Switch to autofield
    
    * Check minor python version
    
    * Fix the workflow
    
    * Fix
    
    * Don't load foreign keys
    
    * Fix everything
    
    * Bump python version
    
    * Downgrade the python version
    
    * Fix the broken pipeline

--- api/app/schemes.py
+++ api/app/schemes.py
@@ -1,11 +1,10 @@
 from app import ma
-from app.models import Lesson, Pair, User
+from app.models import Lesson, Pair, Token, User
 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User
-        ordered = True
 
     id = ma.auto_field(dump_only=True)
     url = ma.String(dump_only=True)
@@ -16,8 +15,6 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
 class LessonSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Lesson
-        include_fk = True
-        load_instance = True
 
     id = ma.auto_field(required=True, load_only=True)
     user_id = ma.auto_field(required=False, load_only=True)
@@ -26,8 +23,11 @@ class LessonSchema(ma.SQLAlchemyAutoSchema):
 class PairSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Pair
-        include_fk = True
-        load_instance = True
 
     id = ma.auto_field(required=True, load_only=True)
     lesson_id = ma.auto_field(required=True, load_only=True)
+
+
+class TokenSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Token
