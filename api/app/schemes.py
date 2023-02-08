6d1afd28a9f7e8ddc69832219c2f152eb46cb61c
Sasha <q.dev.null@gmail.com>
2023-02-08 07:40:41 +0100

    Fix

--- api/app/schemes.py
+++ api/app/schemes.py
@@ -5,7 +5,8 @@ from app.models import Lesson, Pair, User
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User
-        only = ["id", "url", "email"]
+        include_fk = True
+        load_instance = True
 
     id = ma.auto_field(dump_only=True)
     url = ma.String(dump_only=True)
