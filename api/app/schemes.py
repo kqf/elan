da85cbbdb5b3d4fb686320923835bf40a02c63bf
Sasha <q.dev.null@gmail.com>
2023-02-08 07:43:36 +0100

    Don't load foreign keys

--- api/app/schemes.py
+++ api/app/schemes.py
@@ -5,22 +5,20 @@ from app.models import Lesson, Pair, User
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User
-        include_fk = True
-        load_instance = True
+        include_fk = False
+        load_instance = False
 
     id = ma.auto_field(dump_only=True)
     url = ma.String(dump_only=True)
     email = ma.auto_field(required=True)
     password_hash = ma.auto_field(required=True, load_only=True)
-    tokens = ma.auto_field(load_only=True)
-    lessons = ma.auto_field(load_only=True)
 
 
 class LessonSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Lesson
-        include_fk = True
-        load_instance = True
+        include_fk = False
+        load_instance = False
 
     id = ma.auto_field(required=True, load_only=True)
     user_id = ma.auto_field(required=False, load_only=True)
@@ -32,8 +30,8 @@ class LessonSchema(ma.SQLAlchemyAutoSchema):
 class PairSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Pair
-        include_fk = True
-        load_instance = True
+        include_fk = False
+        load_instance = False
 
     id = ma.auto_field(required=True, load_only=True)
     lesson_id = ma.auto_field(required=True, load_only=True)
