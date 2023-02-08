be20b76de6e4fcd9459b6bf62a4398ebefd45652
Sasha <q.dev.null@gmail.com>
2023-02-08 07:47:54 +0100

    Bump python version

--- api/app/schemes.py
+++ api/app/schemes.py
@@ -5,8 +5,6 @@ from app.models import Lesson, Pair, Token, User
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User
-        include_fk = False
-        load_instance = False
 
     id = ma.auto_field(dump_only=True)
     url = ma.String(dump_only=True)
@@ -17,25 +15,17 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
 class LessonSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Lesson
-        include_fk = False
-        load_instance = False
 
     id = ma.auto_field(required=True, load_only=True)
     user_id = ma.auto_field(required=False, load_only=True)
-    pairs = ma.Nested(
-        "PairSchema", many=True, exclude=("lesson",), load_only=True
-    )
 
 
 class PairSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Pair
-        include_fk = False
-        load_instance = False
 
     id = ma.auto_field(required=True, load_only=True)
     lesson_id = ma.auto_field(required=True, load_only=True)
-    lesson = ma.Nested("LessonSchema", exclude=("pairs",), load_only=True)
 
 
 class TokenSchema(ma.SQLAlchemyAutoSchema):
