f32184c0c75108c348984cac407ba52e40982b93
Sasha <q.dev.null@gmail.com>
2023-02-14 07:24:22 +0000

    Rename everything

--- ui/src/components/register.tsx
+++ ui/src/components/register.tsx
@@ -1,137 +1,76 @@
-import { useState } from "react";
+import _ from "lodash";
+import {
+  useForm,
+  FieldError,
+  UseFormRegisterReturn,
+} from "react-hook-form";
 
-interface FormElements extends HTMLFormControlsCollection {
-  username: HTMLInputElement;
-  password: HTMLInputElement;
-  email: HTMLInputElement;
-}
+type RegisterFilds = {
+  username: string;
+  password: string;
+  email: string;
+};
 
-interface UsernameFormElement extends HTMLFormElement {
-  readonly elements: FormElements;
-}
-
-function LoginField(props: {
-  name: string;
+function RegistrationField(props: {
   label: string;
-  error: string;
-  value: string;
-  onChange: (event: React.FormEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  error?: FieldError;
+  inputs: UseFormRegisterReturn;
 }) {
   return (
     <div className="form-group">
-      <label htmlFor={props.name}>{props.label}</label>
-      <input
-        autoFocus
-        value={props.value}
-        onChange={props.onChange}
-        id={props.name}
-        className="form-control"
-        type="text"
-      ></input>
-      {props.error && <div className="alert alert-danger">{props.error}</div>}
-    </div>
+      <label htmlFor={props.inputs.name}>{props.label}</label>
+      <input className="form-control" {...props.inputs} placeholder={props.placeholder}/>
+      {props.error && (
+        <div className="alert alert-danger">{props.error.message}</div>
+      )} </div>
   );
 }
 
 function RegisterForm() {
-  const [state, setState] = useState({
-    account: {
-      username: "John Doe",
-      password: "Default password",
-      email: "joe.doe@gmail.om",
-    },
-    errors: {
-      username: "",
-      password: "",
-      email: "",
-    },
-  });
-
-  const validateProperty = (value: string) => {
-    if (value === "") return "Must not be empty";
-    if (value.length > 20) return "Too long, must be less than 20 characters";
-    return "";
-  };
-
-  const validate = (username: string, password: string, email: string) => {
-    return {
-      status: username.length > 0 && password.length > 0,
-      errors: {
-        username: username.length === 0 ? "Username can't be empty" : "",
-        password: password.length === 0 ? "Password can't be empty" : "",
-        email: email.length === 0 ? "Password can't be empty" : "",
-      },
-    };
-  };
-
-  const handleSubmit = (event: React.FormEvent<UsernameFormElement>) => {
-    event.preventDefault();
-
-    const { status, errors } = validate(
-      event.currentTarget.elements.username.value,
-      event.currentTarget.elements.password.value,
-      event.currentTarget.elements.email.value
-    );
-
-    if (!status) return;
-
-    setState({
-      ...state,
-      errors: errors,
-    });
-  };
-
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    setState({
-      ...state,
-      account: {
-        ...state.account,
-        [event.currentTarget.id]: event.currentTarget.value,
-      },
-      errors: {
-        ...state.errors,
-        [event.currentTarget.id]: validateProperty(event.currentTarget.value),
-      },
-    });
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ mode: "onChange" });
+  const onSubmit = handleSubmit((data) => console.log(data));
 
   return (
     <div>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
-        <LoginField
-          name="email"
-          label="E-mail"
-          value={state.account.email}
-          onChange={handleChange}
-          error={state.errors.email}
+      <h1>Re-login</h1>
+      <form onSubmit={onSubmit}>
+        <RegistrationField
+          label={"Username"}
+          placeholder="Bob"
+          error={errors["username"]}
+          inputs={register("username", {
+            required: "Username is required",
+          })}
         />
-        <LoginField
-          name="password"
-          label="Password"
-          value={state.account.password}
-          onChange={handleChange}
-          error={state.errors.password}
+        <RegistrationField
+          label={"Email"}
+          placeholder="bob@example.com"
+          error={errors["email"]}
+          inputs={register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Please provide a email address"
+            }
+          })}
         />
-        <LoginField
-          name="username"
-          label="Username"
-          value={state.account.username}
-          onChange={handleChange}
-          error={state.errors.username}
+        <RegistrationField
+
+          label={"Password"}
+          placeholder="querty"
+          error={errors["password"]}
+          inputs={register("password", {
+            required: "Password is required",
+          })}
         />
-        <button
-          disabled={
-            !validate(
-              state.account.username,
-              state.account.password,
-              state.account.email
-            ).status
-          }
-          className="btn btn-primary"
-        >
-          Register
+
+        <button disabled={!_.isEmpty(errors)} className="btn btn-primary">
+          Submit
         </button>
       </form>
     </div>
