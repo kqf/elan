848b5ba0ff84876dcc57da1d2465f4a451de1134
Sasha <q.dev.null@gmail.com>
2023-02-16 06:50:52 +0000

    Add the new movie component

---
+++ ui/src/components/newMovie.tsx
@@ -0,0 +1,80 @@
+import _ from "lodash";
+import {
+  useForm,
+  FieldError,
+  UseFormRegisterReturn,
+} from "react-hook-form";
+
+type FormValues = {
+  username: string;
+  password: string;
+  email: string;
+};
+
+function LoginField(props: {
+  label: string;
+  placeholder?: string;
+  error?: FieldError;
+  inputs: UseFormRegisterReturn;
+}) {
+  return (
+    <div className="form-group">
+      <label htmlFor={props.inputs.name}>{props.label}</label>
+      <input className="form-control" {...props.inputs} placeholder={props.placeholder}/>
+      {props.error && (
+        <div className="alert alert-danger">{props.error.message}</div>
+      )} </div>
+  );
+}
+
+function NewMovie() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ mode: "onChange" });
+  const onSubmit = handleSubmit((data) => console.log(data));
+
+  return (
+    <div>
+      <h1>Re-login</h1>
+      <form onSubmit={onSubmit}>
+        <LoginField
+          label={"Username"}
+          placeholder="Bob"
+          error={errors["username"]}
+          inputs={register("username", {
+            required: "Username is required",
+          })}
+        />
+        <LoginField
+          label={"Email"}
+          placeholder="bob@example.com"
+          error={errors["email"]}
+          inputs={register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Please provide a email address"
+            }
+          })}
+        />
+        <LoginField
+
+          label={"Password"}
+          placeholder="querty"
+          error={errors["password"]}
+          inputs={register("password", {
+            required: "Password is required",
+          })}
+        />
+
+        <button disabled={!_.isEmpty(errors)} className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default NewMovie;
