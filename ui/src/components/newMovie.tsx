61978c45142d3ae2acb870dd043d7f30e146979e
Sasha <q.dev.null@gmail.com>
2023-02-19 09:08:53 +0000

    Add the rate field

--- ui/src/components/newMovie.tsx
+++ ui/src/components/newMovie.tsx
@@ -10,6 +10,7 @@ type FormValues = {
   genre: string;
   description: string;
   stock: number;
+  rate: number;
 };
 
 function ErrorField(props: {
@@ -71,6 +72,19 @@ function NewMovie() {
           placeholder="1"
           error={errors["stock"]}
           inputs={register("stock", {
+            required: "Number in stock is required",
+            validate: (val: number) => {
+              if(val < 0 || val >= 100)
+                return "Number in stock should be between 0, 100"
+            }
+          })}
+        />
+
+        <ErrorField
+          label={"Rate"}
+          placeholder="1"
+          error={errors["rate"]}
+          inputs={register("rate", {
             required: "Number in stock is required",
             validate: (val: number) => {
               if(val < 0 || val >= 100)
