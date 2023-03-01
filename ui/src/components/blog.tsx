90464c60cf8db758fb6280101975ac34a354469f
kqf <q.dev.null@gmail.com>
2023-03-01 07:29:04 +0000

    Blog/disable infinite loop (#536)
    * Change the interfacing
    
    * Add the fields
    
    * Add the example
    
    * Add axios
    
    * Fetch the blogposts from API
    
    * Populate the table
    
    * Fix the infinite loop

--- ui/src/components/blog.tsx
+++ ui/src/components/blog.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-
 interface Post {
     userId: number,
     id: number,
@@ -21,13 +20,13 @@ function Posts(props: {
       // Fetch the data
       (async () => {
         const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
-        setState({
+        setState(s => {return {
           ...state,
           posts: response.data
-        });
+        }});
       })();
-
-    })
+      // eslint-disable-next-line
+    }, [])
 
     return (
       <table className="table">
