f12c9d45e451757b244df193af71ed17ff6ec3b7
kqf <q.dev.null@gmail.com>
2023-03-07 12:00:07 +0000

    Movies/add list of genres (#552)
    * Fix
    
    * Refactor the old test
    
    * Fix the test

--- api/tests/routes/test_genre.py
+++ api/tests/routes/test_genre.py
@@ -1,5 +1,23 @@
-def test_retrieves_a_genre(client):
-    response = client.get("/genres/1", follow_redirects=True)
-    assert response.json == {"id": 1, "name": "Action"}
-    # expected = b'{"name":"john","url":"http://localhost/pairs/1"}\n'
+import pytest
+
+
+@pytest.fixture
+def genres():
+    return [
+        {"id": 1, "name": "Action"},
+        {"id": 2, "name": "Comedy"},
+        {"id": 3, "name": "Drama"},
+    ]
+
+
+def test_retrieves_a_genre(client, genres):
+    for i, genre in enumerate(genres):
+        response = client.get(f"/genres/{i + 1}", follow_redirects=True)
+        assert response.json == genre
+        assert response.status_code == 200
+
+
+def test_retrieves_genres(client, genres):
+    response = client.get("/genres/", follow_redirects=True)
+    assert response.json == genres
     assert response.status_code == 200
