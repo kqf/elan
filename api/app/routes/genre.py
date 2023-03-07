b94f27059ef038cb751431c96a5467d9801fa085
Sasha <q.dev.null@gmail.com>
2023-03-07 11:42:19 +0000

    Fix

--- api/app/routes/genre.py
+++ api/app/routes/genre.py
@@ -8,9 +8,16 @@ from app.schemes import GenreSchema
 
 genres = Blueprint("genres", __name__)
 genre_schema = GenreSchema()
+genres_schema = GenreSchema(many=True)
 
 
 @genres.route("/genres/<int:id>", methods=["GET"])
 @response(genre_schema)
 def genre(id) -> Genre:
     return Genre.query.get_or_404(id)
+
+
+@genres.route("/lessons/", methods=["GET"])
+@response(genres_schema)
+def lessons_() -> Genre:
+    return Genre.query.all()
