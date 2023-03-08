455dc3b2209f4b71f6fe78cabd7c82533ccd2450
kqf <q.dev.null@gmail.com>
2023-03-08 08:46:25 +0000

    Movies/simplify tests (#555)
    * Add more examples
    
    * Fix the movie creation code
    
    * Roll back the changes
    
    * Roll back the changes
    
    * Fix the broken code
    
    * Remove the fixture
    
    * Fix the data
    
    * Remove the printouts
    
    * Fix everything

--- api/tests/routes/test_genre.py
+++ api/tests/routes/test_genre.py
@@ -1,23 +1,11 @@
-import pytest
+def test_retrieves_a_genre(client):
+    response = client.get("/genres/1", follow_redirects=True)
+    assert response.json == {"id": 1, "name": "Action"}
+    assert response.status_code == 200
 
 
-@pytest.fixture
-def genres():
-    return [
-        {"id": 1, "name": "Action"},
-        {"id": 2, "name": "Comedy"},
-        {"id": 3, "name": "Drama"},
-    ]
-
-
-def test_retrieves_a_genre(client, genres):
-    for i, genre in enumerate(genres):
-        response = client.get(f"/genres/{i + 1}", follow_redirects=True)
-        assert response.json == genre
-        assert response.status_code == 200
-
-
-def test_retrieves_genres(client, genres):
+def test_retrieves_genres(client):
     response = client.get("/genres/", follow_redirects=True)
-    assert response.json == genres
+    assert len(response.json) == 5
+    assert response.json[0] == {"id": 1, "name": "Action"}
     assert response.status_code == 200
