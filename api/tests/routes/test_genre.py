5205d293a1b06fb035437d3fdbaceeb567b40f0a
Sasha <q.dev.null@gmail.com>
2023-03-08 08:32:18 +0000

    Remove the fixture

--- api/tests/routes/test_genre.py
+++ api/tests/routes/test_genre.py
@@ -1,23 +1,13 @@
 import pytest
 
 
-@pytest.fixture
-def genres():
-    return [
-        {"id": 1, "name": "Action"},
-        {"id": 2, "name": "Comedy"},
-        {"id": 3, "name": "Drama"},
-    ]
-
-
 def test_retrieves_a_genre(client, genres):
-    for i, genre in enumerate(genres):
-        response = client.get(f"/genres/{i + 1}", follow_redirects=True)
-        assert response.json == genre
-        assert response.status_code == 200
+    response = client.get("/genres/1", follow_redirects=True)
+    assert response.json == {"id": 1, "name": "Action"}
+    assert response.status_code == 200
 
 
 def test_retrieves_genres(client, genres):
     response = client.get("/genres/", follow_redirects=True)
-    assert response.json == genres
+    assert len(response.json) == 8
     assert response.status_code == 200
