e882a1bcb256e2dfea3d90f0bdfe71f225ce5771
Sasha <q.dev.null@gmail.com>
2023-03-08 08:33:55 +0000

    Fix the data

--- api/tests/routes/test_genre.py
+++ api/tests/routes/test_genre.py
@@ -1,13 +1,11 @@
-import pytest
-
-
-def test_retrieves_a_genre(client, genres):
+def test_retrieves_a_genre(client):
     response = client.get("/genres/1", follow_redirects=True)
     assert response.json == {"id": 1, "name": "Action"}
     assert response.status_code == 200
 
 
-def test_retrieves_genres(client, genres):
+def test_retrieves_genres(client):
     response = client.get("/genres/", follow_redirects=True)
-    assert len(response.json) == 8
+    assert len(response.json) == 5
+    assert response.json[0] == {"id": 1, "name": "Action"}
     assert response.status_code == 200
