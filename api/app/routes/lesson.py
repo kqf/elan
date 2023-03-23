db6c1a53d344d46325b133c1c82d7e4ae1d7423a
kqf <q.dev.null@gmail.com>
2023-03-23 07:30:42 +0100

    Protect lesson routes (#592)
    * Protect the lesson routes
    
    * Return the user specific lessons
    
    * Fix a broken test
    
    * Remove the redundant route

--- api/app/routes/lesson.py
+++ api/app/routes/lesson.py
@@ -1,32 +1,31 @@
 from __future__ import annotations
 
-from apifairy import response
-from flask import Blueprint, Response
+from apifairy import authenticate, response
+from flask import Blueprint, abort
 
+from app import token_auth
 from app.models import Lesson
-from app.schemes import LessonSchema, PairSchema
+from app.schemes import LessonSchema
 
 lessons = Blueprint("lessons", __name__)
 
 lesson_schema = LessonSchema()
 lessons_schema = LessonSchema(many=True)
-pairs_schema = PairSchema(many=True)
 
 
 @lessons.route("/lessons/", methods=["GET"])
+@authenticate(token_auth)
 @response(lessons_schema)
 def lessons_() -> Lesson:
-    return Lesson.query.all()
+    user = token_auth.current_user()
+    return user.lessons
 
 
 @lessons.route("/lessons/<int:id>", methods=["GET"])
+@authenticate(token_auth)
 @response(lesson_schema)
 def lesson(id) -> Lesson:
-    return Lesson.query.get_or_404(id)
-
-
-@lessons.route("/lessons/<int:id>/data", methods=["GET"])
-@response(pairs_schema)
-def lesson_data(id: int) -> Response:
-    lesson = Lesson.query.get_or_404(id)
-    return lesson.pairs.all()
+    user = token_auth.current_user()
+    if id < 1 or id > len(user.lessons):
+        abort(404)
+    return user.lessons[id - 1]
