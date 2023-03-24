297453cef668b4b27de47e0cc3b35286d73eb9ca
Sasha <q.dev.null@gmail.com>
2023-03-24 09:04:57 +0100

    Fix the lesson

--- api/app/routes/lesson.py
+++ api/app/routes/lesson.py
@@ -48,8 +48,9 @@ def create_lesson() -> tuple[dict, int, dict[str, str]]:
     pairs: list[dict[str, str]] = data["pairs"]  # type: ignore
     # Then create the content
     for pdata in pairs:
-        pair = Pair(lesson_id=lesson.id, **pdata)
+        pair = Pair(**pdata)
         db.session.add(pair)
+        lesson.pairs.append(pair)
         db.session.commit()
 
     user.lessons.append(lesson)
