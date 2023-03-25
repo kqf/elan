8037ddc6a78115a49978ca26f4514db5edcb16ca
kqf <q.dev.null@gmail.com>
2023-03-25 09:11:11 +0100

    Add lesson data (#598)
    * Add lessons
    
    * Add the lesson data

---
+++ ui/src/components/Lesson.tsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import tokenHeader from "../auth";
+
+type LessonParams = {
+  id: string;
+};
+
+export default function Lesson(props?: LessonParams) {
+  const params = useParams<LessonParams>();
+
+  var lesson = params?.id;
+  if (lesson === undefined) {
+    lesson = props?.id;
+  }
+  const navigate = useNavigate();
+  const [state, updateState] = useState([] as Array<any>);
+  useEffect(() => {
+    // Fetch the data
+    (async () => {
+      const header = tokenHeader();
+
+      if (header === null) {
+        navigate("/login");
+        return;
+      }
+
+      const lessons = (
+        await axios.get(`/lessons/${lesson}`, { headers: header })
+      ).data;
+      console.log("fetched ~", lessons);
+      updateState(lessons);
+    })();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div>
+      <ul className="list-group">
+        {state.map((u) => {
+          return (
+            <li key={u.id} className="list-group-item">
+              {u.title}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
