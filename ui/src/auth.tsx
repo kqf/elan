09be97aab946bbada9318553366dfbbd9fd1d9b9
Sasha <q.dev.null@gmail.com>
2023-03-31 07:01:07 +0200

    Add get method

--- ui/src/auth.tsx
+++ ui/src/auth.tsx
@@ -1,4 +1,7 @@
-export default function tokenHeader() {
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export function tokenHeader() {
   if (localStorage.getItem("accessToken") === null) return null;
 
   return {
@@ -6,3 +9,31 @@ export default function tokenHeader() {
     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
   };
 }
+
+axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.resonse.status < 500;
+
+  if (!expectedError) {
+    toast.error("Unexpected error");
+  }
+});
+
+async function get(url: string, redirect?: () => void) {
+  const header = tokenHeader();
+  if (header === null) {
+    redirect && redirect();
+    return;
+  }
+
+  const response = await axios.get(url, { headers: header });
+  console.log("Fetching", url, response.data);
+  return response;
+}
+
+const http = {
+  get: get,
+};
+export default http;
