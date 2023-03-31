42679737b737ed672a72ba74c00a9790d9c58493
Sasha <q.dev.null@gmail.com>
2023-03-31 06:43:43 +0200

    Fix the error notifications

--- ui/src/components/Lessons.tsx
+++ ui/src/components/Lessons.tsx
@@ -1,7 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import tokenHeader from "../auth";
+
+axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.resonse.status < 500;
+
+  if (!expectedError) {
+    console.log("Logging the error", error);
+    toast.error("Unexpected error");
+  }
+}
 
 export default function Lessons() {
   const navigate = useNavigate();
@@ -15,15 +28,22 @@ export default function Lessons() {
         return;
       }
 
-      const lessons = (await axios.get("/lessons/", { headers: header })).data;
-      console.log("fetched ~", lessons);
-      updateState(lessons);
+      try {
+        const lessons = (await axios.get("/lessons/", { headers: header }))
+          .data;
+        console.log("fetched ~", lessons);
+        updateState(lessons);
+      } catch (error) {
+        toast.error("Ooops, something went wrong.");
+        return;
+      }
     })();
     // eslint-disable-next-line
   }, []);
 
   return (
     <div>
+      <ToastContainer />
       <ul className="list-group">
         {state.map((u) => {
           return (
