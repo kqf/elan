3adc2b46558f0c4b8f925cd1e15891ed001daedf
Sasha <q.dev.null@gmail.com>
2023-04-10 08:45:01 +0200

    Add the missing libraries

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 import { useEffect, useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
