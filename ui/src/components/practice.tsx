e15fd519828db9bc73bfb654205489a2b51beba6
Sasha <q.dev.null@gmail.com>
2023-06-07 08:11:54 +0200

    Handle the empty lesson

--- ui/src/components/practice.tsx
+++ ui/src/components/practice.tsx
@@ -1,11 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 type PracticeParams = {
   id?: string;
 };
 
 function Practice() {
+  const navigate = useNavigate();
   const lessonId = useParams<PracticeParams>().id;
+  if (lessonId === undefined) {
+    navigate("/lessons");
+  }
   return <h1>Excercise your skills for lesson {lessonId}</h1>;
 }
 
