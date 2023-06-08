734a1b074a3b272896689184d6583c707eac2884
kqf <q.dev.null@gmail.com>
2023-06-08 10:40:09 +0200

    Add the practice form (#707)

--- ui/src/components/practice.tsx
+++ ui/src/components/practice.tsx
@@ -1,8 +1,34 @@
+import { FieldError, UseFormRegisterReturn, useForm } from "react-hook-form";
 import { useNavigate, useParams } from "react-router-dom";
 
 type PracticeParams = {
   id?: string;
 };
+
+type FormValues = {
+  answer: string;
+};
+
+function ErrorField(props: {
+  label: string;
+  placeholder?: string;
+  error?: FieldError;
+  inputs: UseFormRegisterReturn;
+}) {
+  return (
+    <div className="form-group">
+      <label htmlFor={props.inputs.name}>{props.label}</label>
+      <input
+        className="form-control"
+        {...props.inputs}
+        placeholder={props.placeholder}
+      />
+      {props.error && (
+        <div className="alert alert-danger">{props.error.message}</div>
+      )}{" "}
+    </div>
+  );
+}
 
 function Practice() {
   const navigate = useNavigate();
@@ -10,7 +36,28 @@ function Practice() {
   if (lessonId === undefined) {
     navigate("/lessons");
   }
-  return <h1>Excercise your skills for lesson {lessonId}</h1>;
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ mode: "onChange" });
+
+  return (
+    <div>
+      <h1>Excercise your skills for lesson {lessonId}</h1>
+      <form>
+        <ErrorField
+          label={"INPUT"}
+          placeholder="Answer"
+          error={errors["answer"]}
+          inputs={register("answer", {
+            required: "Please provide the lesson level",
+          })}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default Practice;
