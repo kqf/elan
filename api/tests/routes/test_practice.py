29d4c2787a57110eb13f7b40bfb82155193000cb
Sasha <q.dev.null@gmail.com>
2023-06-09 14:17:03 +0200

    Fix the broken test

--- api/tests/routes/test_practice.py
+++ api/tests/routes/test_practice.py
@@ -1,8 +1,7 @@
-import app.models as users
-
-
 def test_retrieves_users(client, headers):
     response = client.get(
-        "/practice/1", headers=headers, follow_redirects=True
+        "/practice/1",
+        headers=headers,
+        follow_redirects=True,
     )
     assert response.status_code == 200
