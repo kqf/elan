3cbfddfee4bf7dfee29b93dcf530a1896766eaef
Sasha <q.dev.null@gmail.com>
2023-06-09 14:06:17 +0200

    Fix the Practice Lesson id

--- api/app/models.py
+++ api/app/models.py
@@ -132,9 +132,18 @@ class User(db.Model):
 
 
 class PracticeLesson(db.Model):
-    __tablename__ = "practicelesson"
-    lesson_id = db.Column(db.integer, primary_key=True)
-    pair_id = db.Column(db.integer, primary_key=True)
+    __tablename__ = "practice_lesson"
+    id = db.Column(db.Integer, primary_key=True)
+    lesson_id = db.Column(
+        db.Integer,
+        db.ForeignKey("lessons.id"),
+        nullable=True,
+    )
+    pair_id = db.Column(db.Integer, db.ForeignKey("pairs.id"), nullable=True)
+    user = db.relationship("User", back_populates="practice_lesson")
+
+    lesson = db.relationship("Lesson")
+    pair = db.relationship("Pair")
 
 
 class Lesson(db.Model):
