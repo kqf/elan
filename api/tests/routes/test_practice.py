101bfc5b69fb5f45e6a2090949b47276c8ca7752
kqf <q.dev.null@gmail.com>
2023-06-10 13:55:54 +0200

    Add practice tests (#712)
    * Fix the practice
    
    * Fix the broken test

---
+++ api/tests/routes/test_practice.py
@@ -0,0 +1,7 @@
+def test_retrieves_users(client, headers):
+    response = client.get(
+        "/practice/1",
+        headers=headers,
+        follow_redirects=True,
+    )
+    assert response.status_code == 200
