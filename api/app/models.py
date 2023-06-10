ec2efaba44084de694d733d002eafc4e718dd9b4
kqf <q.dev.null@gmail.com>
2023-06-10 14:12:02 +0200

    Fix more typos (#714)
    * Fix the practice
    
    * Fix the broken test
    
    * Fix the typos
    
    * Fix more typos

--- api/app/models.py
+++ api/app/models.py
@@ -134,7 +134,7 @@ class User(db.Model):
 class PracticeLesson(db.Model):
     __tablename__ = "practicelesson"
     lesson_id = db.Column(db.Integer, primary_key=True)
-    pair_id = db.Column(db.integer, primary_key=True)
+    pair_id = db.Column(db.Integer, primary_key=True)
 
 
 class Lesson(db.Model):
