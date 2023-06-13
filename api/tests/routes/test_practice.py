f9b3539d2b7d5063aebfcfac72048e0022e95713
kqf <q.dev.null@gmail.com>
2023-06-13 10:18:23 +0200

    Fix all broken tests (#720)
    * Fix the mistyped table name
    
    * Fix more
    
    * Fix misspelled typos again
    
    * Fix the foreign key dependence
    
    * Fix the foreign keys interdependences
    
    * Add the missing blueprint
    
    * Fix the misspelled branch
    
    * Add the missing lessons
    
    * Fix the missing imports
    
    * Add the missing fixtures

--- api/tests/routes/test_practice.py
+++ api/tests/routes/test_practice.py
@@ -1,4 +1,43 @@
-def test_retrieves_users(client, headers):
+import pytest
+
+from app import db
+from app.models import Lesson, Pair, User
+
+
+@pytest.fixture
+def example_data():
+    return [
+        {
+            "iffield": "la vache",
+            "offield": "the cow",
+        },
+        {
+            "iffield": "le monde",
+            "offield": "the world",
+        },
+    ]
+
+
+@pytest.fixture
+def example(client, example_data):
+    users = User.query.all()
+    lesson = Lesson(title="lesson 1")
+    db.session.add(lesson)
+    db.session.commit()
+
+    pairs = [Pair(**p) for p in example_data]
+    for p in pairs:
+        db.session.add(p)
+        lesson.pairs.append(p)
+        db.session.commit()
+
+    for user in users:
+        user.lessons.append(lesson)
+        db.session.commit()
+    return pairs
+
+
+def test_retrieves_users(client, headers, example):
     response = client.get(
         "/practice/1",
         headers=headers,
