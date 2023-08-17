03993681cdafa270341151cb6ed0731836283de5
kqf <q.dev.null@gmail.com>
2023-08-17 08:59:15 +0100

    Remove movie scheme (#832)
    * Fix the navbar
    
    * Remove the new movie
    
    * Remove the movie schema

--- ui/src/schemes.tsx
+++ ui/src/schemes.tsx
@@ -3,16 +3,6 @@ export interface Genre {
   name: string;
 }
 
-export interface Movie {
-  id: string;
-  title: string;
-  genre: Genre;
-  numberInStock: number;
-  dailyRentalRate: number;
-  publishDate: string;
-  liked: boolean;
-}
-
 export interface User {
   id: string;
   email: string;
